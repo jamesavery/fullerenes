beefdf2b61989b00c8006e0390c4371ef910eb26
James Avery <avery@diku.dk>
2012-04-10 15:04:38 +1200

    Graph->mathematica print program

--- tests/print.cc
+++ tests/print.cc
@@ -1,10 +1,20 @@
 #include "libgraph/fullerenegraph.hh"
+#include "libgraph/polyhedron.hh"
 
-int main()
+int main(int ac, char **av)
 {
-  FullereneGraph g(stdin);
-  g.layout2d = g.tutte_layout();
+  CubicGraph g(stdin);
+
+  if(ac>=2 && string(av[1]) == "do_layout")
+    g.layout2d = g.tutte_layout();
+
+  bool is_fullerene = g.this_is_a_fullerene();
+  cerr << "Graph is "<<(is_fullerene?"":"not ")<<"a fullerene.\n";
+
   cout << "graph = " << g << ";\n";
+  vector<coord3d> points(Polyhedron::polar_mapping(g.spherical_projection()));
+  
+  cout << "points = {"; for(int i=0;i<points.size();i++) cout << points[i] << (i+1<points.size()?",":"};\n");
   //  cout << "dual  = " << g.dual_graph() << ";\n";
 
   return 0;
