0b7adae05ac1a2b4d8ae4f7c9e55a3913bf830fb
Peter Schwerdtfeger <paschwer@Users-MacBook-Pro-2.local>
2012-12-22 15:34:59 +1300

    Added more statistics in spiral.f

--- webpage/fullerdl.php
+++ webpage/fullerdl.php
@@ -4,7 +4,7 @@ The following password needs to be identical to
 the one in defined in file    userdata.php
 */
 
-$file='data/fullerenev4.3.zip';
+$file='data/fullerenev4.3.1.zip';
 $passwd = $_GET["pwd"];
 
 if ($passwd == "R4TY19KVBN")
@@ -16,7 +16,7 @@ if ($passwd == "R4TY19KVBN")
         //content type
         header('Content-type: application/zip');
         //open/save dialog box
-        header('Content-Disposition: attachment; filename="fullerenev4.3.zip"');
+        header('Content-Disposition: attachment; filename="fullerenev4.3.1.zip"');
         //read from server and write to buffer
         readfile($file);
   }
