eb57ac139687a3f822e1dc053d9b4f8035eb8e9f
Peter Schwerdtfeger <paschwer@Users-MacBook-Pro-2.local>
2013-01-20 09:23:02 +1300

    Added the FuiGui paper into the JCC paper

--- webpage/fullerdl.php
+++ webpage/fullerdl.php
@@ -4,7 +4,7 @@ The following password needs to be identical to
 the one in defined in file    userdata.php
 */
 
-$file='data/fullerenev4.3.1.zip';
+$file='data/fullerenev4.4.zip';
 $passwd = $_GET["pwd"];
 
 if ($passwd == "R4TY19KVBN")
@@ -16,7 +16,7 @@ if ($passwd == "R4TY19KVBN")
         //content type
         header('Content-type: application/zip');
         //open/save dialog box
-        header('Content-Disposition: attachment; filename="fullerenev4.3.1.zip"');
+        header('Content-Disposition: attachment; filename="fullerenev4.4.zip"');
         //read from server and write to buffer
         readfile($file);
   }
