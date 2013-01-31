32e476257351ba642ad32b600a78c923857c3dd5
Lukas Wirz <l.wirz@massey.ac.nz>
2013-01-31 16:49:21 +1300

    some changes to general spiral but still not fucntional/general

--- tests/general_spiral.cc
+++ tests/general_spiral.cc
@@ -9,15 +9,16 @@ int main(int ac, char **av)
   std::cout << "Attempting to create graph from spiral indices ..." << std::endl;
  
   int n = 192;
+//  int n = 16;
 //no jump
-//  int pentagon_indices[] = {1, 2, 3, 4, 5, 7, 10, 12, 13, 14, 15, 16};
+//  int pentagon_indices_array[] = {1, 2, 3, 4, 5, 7, 10, 12, 13, 14, 15, 16};
 //jump
-//  int pentagon_indices[] = {1, 2, 3, 5, 8, 9, 11, 12, 13, 14, 15, 16};
+//  int pentagon_indices_array[] = {1, 2, 3, 5, 8, 9, 11, 12, 13, 14, 15, 16};
 //jump, 380
   int pentagon_indices_array[] = {1, 2, 3, 137, 138, 147, 148, 157, 158, 166, 173, 180};
   std::vector<int> pentagon_indices_input(12);
   std::vector<int> pentagon_indices_output;
-  std::vector<int> jumps(200,0);
+  std::vector<pair<int,int> > jumps;
   bool ipr=false;
   bool general=true;
 
