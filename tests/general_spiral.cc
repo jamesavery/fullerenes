e40154c2f8cb831bc2f919973106fb62fe8e4b7a
Lukas Wirz <l.wirz@massey.ac.nz>
2013-02-17 15:15:02 +1300

    a bit of refinement in general spiral (including the understanding that one bug was actually wrong input)

--- tests/general_spiral.cc
+++ tests/general_spiral.cc
@@ -17,15 +17,15 @@ int main(int ac, char **av)
 //  int n = 28;
 //  int pentagon_indices_array[] = {0, 1, 2, 4, 7, 8, 10, 11, 12, 13, 14, 15};
 //jump, 380
-  int n = 380;
-  int pentagon_indices_array[] = {0, 1, 2, 136, 137, 146, 147, 156, 157, 165, 172, 179};
-  int jumps_array[][2] = {{165,2}, {172,3}, {179,3}};
-  int n_jumps = 3;
+//  int n = 380;
+//  int pentagon_indices_array[] = {0, 1, 2, 136, 137, 146, 147, 156, 157, 165, 172, 179};
+//  int jumps_array[][2] = {{165,2}, {172,3}, {179,3}};
+//  int n_jumps = 3;
 //jump, 440
-//  int n = 440;
-//  int pentagon_indices_array[] = {159, 160, 161, 174, 175, 176, 189, 190, 191, 195, 204, 212};
-//  int jumps_array[][2] = {{192,6}, {195,6}, {204,6}, {212,7}};
-//  int n_jumps = 4;
+  int n = 440;
+  int pentagon_indices_array[] = {159, 160, 161, 174, 175, 176, 189, 190, 191, 195, 204, 212};
+  int jumps_array[][2] = {{192,6}, {195,6}, {204,6}, {212,7}};
+  int n_jumps = 4;
 
   vector<int> pentagon_indices_input(12);
   vector<int> pentagon_indices_output;
@@ -66,19 +66,19 @@ int main(int ac, char **av)
 
   jumps.clear();
   pentagon_indices_output.clear();
-  fg.get_pentagon_indices(0, 1, 2, pentagon_indices_output, jumps);  
-//
-//  cout << "Output spiral indices: ";
-//  for (int i=0; i<12; ++i){
-//    cout <<  pentagon_indices_output[i] << ", ";
-//  }
-//  cout << endl;
-//  
-//  cout << "Jumps: ";
-//  for (list<pair<node_t,int> >::iterator it=jumps.begin(); it!=jumps.end(); ++it){
-//    cout <<  it->first << ": " << it->second << ", ";
-//  }
-//  cout << endl;
+  fg.get_pentagon_indices(0, 1, 9, pentagon_indices_output, jumps);  
+
+  cout << "Output spiral indices: ";
+  for (int i=0; i<12; ++i){
+    cout <<  pentagon_indices_output[i] << ", ";
+  }
+  cout << endl;
+  
+  cout << "Jumps: ";
+  for (list<pair<node_t,int> >::iterator it=jumps.begin(); it!=jumps.end(); ++it){
+    cout <<  it->first << ": " << it->second << ", ";
+  }
+  cout << endl;
 
   return 0;
 }
