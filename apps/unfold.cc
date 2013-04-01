9cf53da83a810b91ac9bc513ff2f33326366331d
James Avery <avery@diku.dk>
2013-04-01 12:47:04 +0200

    Unfolding triangulations of degree <= 6 onto the equilateral plane now works!

--- apps/unfold.cc
+++ apps/unfold.cc
@@ -39,19 +39,20 @@ public:
   coord2d coord() const { return coord2d(1,0)*first + coord2d(0.5,0.8660254037844386)*second; }
 };
 
+typedef pair<Eisenstein,Eisenstein> dedgecoord_t;
 
 // Preconditions: Triangles are oriented consistently
-map<Eisenstein,node_t> unfold(const vector<tri_t> &triangulation)
+map<dedge_t,dedgecoord_t> unfold(const vector<tri_t> &triangulation)
 {
 #define set_dedge(u,v,ux,vx) {	          \
   dedge_t uv(u,v), vu(v,u);               \
   printf("set_dedge(%d,%d)\n",u,v);       \
   dedge_done[uv] = true;                  \
   workset.erase(uv);                      \
+  dedge_position[vu] = make_pair(vx,ux);  \
   if(!dedge_done[vu]){                    \
     workset.insert(vu);			  \
-    dedge_position[vu] = make_pair(vx,ux);\
-  }                                       \
+  } \
 }
 
   map<dedge_t,node_t> nextNode;
@@ -63,44 +64,85 @@ map<Eisenstein,node_t> unfold(const vector<tri_t> &triangulation)
 
   map<dedge_t,bool> dedge_done;
   set<dedge_t>   workset;
-  map<dedge_t, pair<Eisenstein,Eisenstein> > dedge_position;
+  map<dedge_t, dedgecoord_t > dedge_position;
 
   map<Eisenstein,node_t> grid;
   Eisenstein zero(0,0), veci(1,0), vecj(0,1);
 
   // 1. Place first triangle. 
   tri_t t(triangulation[0]);
-  cout << "Place " << t[0] << " at " << zero << endl;
-  cout << "Place " << t[1] << " at " << veci << endl;
-  cout << "Place " << t[2] << " at " << (veci-vecj) << endl;
-  grid[zero]      = t[0];
-  grid[veci]      = t[1];
-  grid[veci-vecj] = t[2];
 
   set_dedge(t[0],t[1],zero,veci);
   set_dedge(t[1],t[2],veci,veci-vecj);
   set_dedge(t[2],t[0],veci-vecj,zero);
 
   while(!workset.empty()){
-    dedge_t uv(*workset.begin());
-    cout << "Next unused dedge is " << uv << endl;
+    dedge_t uv(*workset.rbegin());
+    //    cout << "Next unused dedge is " << uv << endl;
     // set_triangle(uv)
     node_t u(uv.first), v(uv.second), w(nextNode[uv]);
 
-    pair<Eisenstein,Eisenstein> uvpos(dedge_position[uv]);
+    dedgecoord_t uvpos(dedge_position[uv]);
     Eisenstein ux(uvpos.first), vx(uvpos.second), wx(ux+Eisenstein::nextCW(ux,vx));
-
-    cout << "Place " << w << " at " << wx << endl;
-    grid[wx] = w;
 
     set_dedge(u,v,ux,vx);
     set_dedge(v,w,vx,wx);
     set_dedge(w,u,wx,ux);
 
   }
-  return grid;
+  return dedge_position;
 }
 
+vector< pair<Eisenstein,node_t> > get_outline(const map<dedge_t,dedgecoord_t>& edgecoords)
+{
+  map<Eisenstein,node_t>    label;
+  map<Eisenstein,Eisenstein> next;
+
+  // Collect the directed edges u->v whose positions do not coincide with the reverse edge v->u. 
+  // These form the outline of the polygon. 
+  for(map<dedge_t,dedgecoord_t>::const_iterator i(edgecoords.begin()); i!= edgecoords.end(); i++){
+    const dedge_t &uv(i->first), vu(uv.second,uv.first);
+    const dedgecoord_t &uvpos(i->second), vupos(edgecoords.find(vu)->second);
+
+    if(uvpos != make_pair(vupos.second,vupos.first)){
+      next[uvpos.first]   = uvpos.second;
+      label[uvpos.first]  = uv.first;
+    }
+  }
+  
+   // Now we're ready to find a CW walk through the polygon outline coordinates 
+  // and to assign the corresponding nodes in the triangulation graph as labels.
+  vector< pair<Eisenstein,node_t> > outline(next.size());
+  Eisenstein nextpos = next.begin()->first;
+
+  for(int i=0;i<outline.size();i++, nextpos = next[nextpos]){
+    cout << i << ": " << label[nextpos] << " at " << nextpos << endl;
+    outline[i] = make_pair(nextpos,label[nextpos]);
+  }
+
+  // If the outline doesn't form a closed loop, something is wrong.
+  assert(nextpos == next.begin()->first);
+
+  return outline;
+}
+
+template<typename K, typename V> vector<K> get_keys(const map<K,V>& m)
+{
+  vector<K> keys(m.size());
+  int i=0;
+  for(typename map<K,V>::const_iterator kv(m.begin()); kv!=m.end(); kv++,i++)
+    keys[i] = kv->first;
+  return keys;
+}
+
+template<typename K, typename V> vector<V> get_values(const map<K,V>& m)
+{
+  vector<V> values(m.size());
+  int i=0;
+  for(typename map<K,V>::const_iterator kv(m.begin()); kv!=m.end(); kv++,i++)
+    values[i] = kv->second;
+  return values;
+}
 
 int main(int ac, char **av)
 {
@@ -114,6 +156,7 @@ int main(int ac, char **av)
 
   cout << "Attempting to create graph from spiral indices " << rspi << endl;
  
+  ofstream output("output/C"+to_string(N)+"-unfold.m");
 
   FullereneGraph g(N, rspi, jumps);
   PlanarGraph dual(g.dual_graph(6));
@@ -121,19 +164,24 @@ int main(int ac, char **av)
   g.layout2d = g.tutte_layout();
   dual.layout2d = dual.tutte_layout();
 
-  cout << "g = "  << g << ";\n";
-  cout << "dg = " << dual << ";\n";
+  output << "g = "  << g << ";\n";
+  output << "dg = " << dual << ";\n";
+  cout << "Need to place 2x"<<dual.edge_set.size()<<" edges.\n";
 
   vector<face_t> faces(dual.compute_faces_flat(3,true));
   vector<tri_t>  triangles(faces.begin(),faces.end());
 
-  map<Eisenstein,node_t> grid(unfold(triangles));
+  map<dedge_t,dedgecoord_t>         grid(unfold(triangles));
 
-  for(map<Eisenstein,node_t>::const_iterator i(grid.begin()); i!=grid.end();i++){
-    Eisenstein x(i->first);
-    node_t u(i->second);
-    cout << u << " at " << x << endl;
-  }
+  cout << "Placed " << grid.size() << " edges.\n";
+
+  output << "dedges   = " << get_keys(grid) << ";\n";
+  output << "dedgepos = " << get_values(grid) << ";\n";
+
+  vector< pair<Eisenstein,node_t> > outline(get_outline(grid));
+  output << "outline = " << outline << ";\n";
+
+  output.close();
   
   return 0;
 }
