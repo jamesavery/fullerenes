c7cd72c0c0edf25d4feccc19fc027f5c812e7f5a
Lukas Wirz <l.wirz@massey.ac.nz>
2013-05-03 10:00:14 +1200

    print pent dist output should conform with mathematica

--- apps/print-pent-distances.cc
+++ apps/print-pent-distances.cc
@@ -12,8 +12,10 @@ int main(int ac, char **av)
 // spiral indices are input starting at 0
 //  int n = 20;
 //  int pentagon_indices_array[] = {0,1,2,3,4,5,6,7,8,9,10,11};
-  int n = 32;
-  int pentagon_indices_array[] = {0,1,2,3,6,9,10,12,13,15,16,17 };
+//  int n = 32;
+//  int pentagon_indices_array[] = {0,1,2,3,6,9,10,12,13,15,16,17 };
+  int n = 60;
+  int pentagon_indices_array[] = {2, 4, 6, 8, 11, 14, 16, 19, 22, 26, 28, 30};
   int jumps_array[][2] = {{171,7}};
   int n_jumps = 0;
 
@@ -33,19 +35,15 @@ int main(int ac, char **av)
   cout << endl;
   cout << endl;
 
-  //100 times 0 to make sure size() is large
-  vector<int> general_spiral_bak(100,0);
-
   FullereneGraph fg(n, pentagon_indices_input, jumps);
   PlanarGraph dual(fg.dual_graph(6));
-//  vector<face_t> faces(dual.compute_faces_flat(3));
 
   vector<int> pentagons;
   for(int i=0;i<dual.N;i++) if(dual.neighbours[i].size() == 5) pentagons.push_back(i);
-
   cout << "pentagon indices: (normally 0 through 11)" << endl;
   cout << pentagons << endl<<endl;
 
+  // I know the following is very wasteful but it's good enough
   vector<int> all_distances = dual.all_pairs_shortest_paths(INT_MAX);
   vector<int> pentagon_distances(144, 0);
 
@@ -63,7 +61,9 @@ int main(int ac, char **av)
       cout << pentagon_distances[12*i + j];
       if(j!=11)cout << ", ";
     }
-    cout << "}" << endl;
+    cout << "}";
+    if(i!=11)cout << ",";
+    cout << endl;
   }
   cout << "}" << endl;
 
