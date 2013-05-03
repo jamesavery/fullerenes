c9f91c4aba469fdd092faa9c4cacbcbaad0847f9
Lukas Wirz <l.wirz@massey.ac.nz>
2013-05-03 15:16:44 +1200

    external input would be better.  jumps dont work here

--- apps/print-pent-distances.cc
+++ apps/print-pent-distances.cc
@@ -6,37 +6,21 @@ using namespace std;
 
 int main(int ac, char **av)
 {
+  vector<int> rspi(12);
+  FullereneGraph::jumplist_t jumps;
 
-  std::cout << "Attempting to create graph from spiral indices ..." << std::endl;
+  if(ac != 14) return -1;
+
+  int N = atol(av[1]);
+  cout << N << endl;
+  for(int i=0;i<12;i++) rspi[i] = strtol(av[2+i],0,0)-1;
+
+  cout << "Attempting to create graph from spiral indices " << rspi << endl;
  
-// spiral indices are input starting at 0
-//  int n = 20;
-//  int pentagon_indices_array[] = {0,1,2,3,4,5,6,7,8,9,10,11};
-//  int n = 32;
-//  int pentagon_indices_array[] = {0,1,2,3,6,9,10,12,13,15,16,17 };
-  int n = 60;
-  int pentagon_indices_array[] = {2, 4, 6, 8, 11, 14, 16, 19, 22, 26, 28, 30};
-  int jumps_array[][2] = {{171,7}};
-  int n_jumps = 0;
 
-  vector<int> pentagon_indices_input(pentagon_indices_array,pentagon_indices_array+12);
-  vector<int> pentagon_indices_output;
-  vector<int> spiral;
-  list<pair<int,int> > jumps;
-  
-  cout << "N: " << n << endl
-       << "Input spiral indices: " << pentagon_indices_input << endl;
+  FullereneGraph g(N, rspi, jumps);
+  PlanarGraph dual(g.dual_graph(6));
 
-  cout << "Input jumps: ";
-  for (int i=0; i<n_jumps; ++i){
-    jumps.push_back(make_pair(jumps_array[i][0], jumps_array[i][1]));
-    cout <<  jumps.rbegin()->first << ", " << jumps.rbegin()->second << "; " ;
-  }
-  cout << endl;
-  cout << endl;
-
-  FullereneGraph fg(n, pentagon_indices_input, jumps);
-  PlanarGraph dual(fg.dual_graph(6));
 
   vector<int> pentagons;
   for(int i=0;i<dual.N;i++) if(dual.neighbours[i].size() == 5) pentagons.push_back(i);
