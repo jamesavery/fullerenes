10e78c16aef37281d040266f66ec17bff5cf357e
James Avery <avery@diku.dk>
2013-07-26 16:47:15 +0200

    First attempt at implementing GCD for Eisenstein numbers.

--- libgraph/eisenstein.hh
+++ libgraph/eisenstein.hh
@@ -1,6 +1,7 @@
 #ifndef EISENSTEIN_HH
 # define EISENSTEIN_HH
 
+#include <stdlib.h>
 #include "geometry.hh"
 using namespace std;
 
@@ -30,12 +31,18 @@ public:
   }
   Eisenstein operator+(const Eisenstein& y) const { return Eisenstein(first+y.first,second+y.second); }
   Eisenstein operator-(const Eisenstein& y) const { return Eisenstein(first-y.first,second-y.second); } 
+  coord2d    operator-(const coord2d& y)    const { return coord2d(first-y.first,second-y.second); } 
+  coord2d    operator+(const coord2d& y)    const { return coord2d(first+y.first,second+y.second); } 
   Eisenstein& operator+=(const Eisenstein& y) { first += y.first; second += y.second; return *this; }
   Eisenstein& operator-=(const Eisenstein& y) { first -= y.first; second -= y.second; return *this; }
 
   // invertn(a,b) * (a,b) == norm2() (1,0)
   Eisenstein invertn() const { return Eisenstein((first+second), -second); }
   Eisenstein GCtransform(int k, int l) const {  return Eisenstein(k,l) * (*this);  }
+  Eisenstein affine(const Eisenstein& x0, const Eisenstein w) const {
+    //    cout << x0 << " + " << w << " * " << *this << " = " << (x0+(w*(*this))) <<endl;
+    return x0+(w*(*this));
+  }
   //  
   // (-1,1)   \ /  (0,1)
   // (-1,0)  --x-- (1,0)
@@ -44,27 +51,49 @@ public:
   Eisenstein nextCW() const { return (*this) * Eisenstein(1,-1); }
   Eisenstein nextCCW() const { return (*this) * Eisenstein(0,1); }
 
-  Eisenstein nextCWold() const {
-    switch(second + 10*first){
-    case  10+0: /*( 1 ,0)*/  return Eisenstein(1,-1);
-    case  10-1: /*( 1,-1)*/  return Eisenstein(0,-1); 
-    case   0-1: /*( 0,-1)*/  return Eisenstein(-1,0);
-    case -10+0: /*(-1, 0)*/  return Eisenstein(-1,1);
-    case -10+1: /*(-1, 1)*/  return Eisenstein(0,1);
-    case   0+1: /*( 0, 1)*/  return Eisenstein(1,0);
-    default:
-      cerr << "nextCW(): " << *this << " is not a grid direction.\n";
-      abort();
-    }
- }
-
   coord2d coord() const { return coord2d(1,0)*first + coord2d(1/2.,sqrt(3/4.))*second; }
 
   int norm2() const { return first*first + first*second + second*second; }
   double norm() const { return sqrt(norm2()); }
 
-  Eisenstein GCD(const Eisenstein &y) const;
+  Eisenstein abs() const { return Eisenstein(::abs(first),::abs(second)); }
 
+  Eisenstein div(const Eisenstein& y) const {
+    // Naive, possibly non-robust algorithm
+    Eisenstein z(*this * y.invertn());
+    coord2d zf(z.first,z.second);
+    zf *= 1.0/y.norm2();
+
+    return Eisenstein(round(zf.first),round(zf.second));
+  }
+
+  Eisenstein mod(const Eisenstein& y) const {
+    Eisenstein q(this->div(y));
+    return (*this) - q*y;
+  }
+
+  static coord2d average(const vector<Eisenstein>& xs)
+  {
+    coord2d sum(0,0);
+    for(int i=0;i<xs.size();i++) sum += coord2d(xs[i].first,xs[i].second);
+    return sum / xs.size(); 
+  }
+
+  static Eisenstein gcd(Eisenstein a, Eisenstein b)  {
+    Eisenstein c;
+    while ( a != Eisenstein(0,0) ) {
+      c = a; a = b.mod(a);  b = c;
+    }
+    return b;
+  }
+
+  static Eisenstein gcd(const vector<Eisenstein>& xs) {
+    if(xs.empty()) return Eisenstein(1,0);
+
+    Eisenstein d(xs[0]);
+    for(int i=1;i<xs.size();i++) d = gcd(xs[i],d);
+    return d;
+  }
 };
 
 
