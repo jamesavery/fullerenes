d0653eed2bb314b0e049c60dd32ee41856f6c25b
Lukas Wirz <l.wirz@massey.ac.nz>
2013-09-19 18:03:48 +0200

    traingulation constructor from general spiral (optimisation pending)

--- libgraph/triangulation.cc
+++ libgraph/triangulation.cc
@@ -112,7 +112,8 @@ void Triangulation::orient_neighbours()
   }
 }
 
-// Takes full spiral string, e.g. 5666436634665 
+// Takes full spiral string, e.g. 566764366348665
+// where the degrees are between 3 and 8 
 Triangulation::Triangulation(const vector<int>& spiral_string)
 {
   N = spiral_string.size();
@@ -121,8 +122,119 @@ Triangulation::Triangulation(const vector<int>& spiral_string)
 }
 
 
-Triangulation::Triangulation(const vector<int>& spiral_string, const jumplist_t jumps)
+void wg_connect_backward(set<edge_t> &edge_set, list<pair<node_t, int> > &ov)
 {
-  N = spiral_string.size();
-  // Lukas: Can you implement a fast general spiral windup?
+  list< pair<node_t,int> >::iterator second_last(ov.end());
+  --second_last;
+  --second_last;
+
+  edge_set.insert(edge_t(ov.back().first, second_last->first));
+  --ov.back().second;
+  --(second_last->second);//decrement the last but one entry
 }
+
+void wg_connect_forward(set<edge_t> &edge_set, list<pair<node_t, int> > &ov)
+{
+  edge_set.insert(edge_t(ov.back().first, ov.front().first));
+  --ov.back().second;
+  --ov.front().second;
+}
+
+// // debug only (do not remove, please [lukas])
+// void pdp(list<pair<int,int> > &open_valencies){
+//   for(list<pair<int,int> >::iterator it(open_valencies.begin()); it!= open_valencies.end(); ++it){
+//      cout << it->first << ": " << it->second << endl;
+//   }
+// }
+
+Triangulation::Triangulation(const vector<int>& spiral_string, const jumplist_t& j): PlanarGraph()
+{
+  jumplist_t jumps = j; // we need a local copy to remove elements
+  N = spiral_string.size();
+
+  set<edge_t> edge_set;
+
+  // open_valencies is a list with one entry per node that has been added to
+  // the spiral but is not fully saturated yet.  The entry contains the number
+  // of the node and the number of open valencies
+  list<pair<int,int> > open_valencies;
+
+  // set up first two nodes
+  open_valencies.push_back(make_pair(0,spiral_string[0]));
+  open_valencies.push_back(make_pair(1,spiral_string[1]));
+  //connect first two faces
+  wg_connect_backward(edge_set, open_valencies);
+
+  //iterate over atoms
+  //k=0, k=1 have been done already
+  // omet the last one because it requires special treatment
+  for (int k=2; k<N-1; ++k){
+//    cout << "k: " << k << endl;
+
+    if(jumps.size() != 0 && k == jumps.front().first){
+      // perform cyclic shift on open_valencies
+      for(int i = jumps.front().second; i>0; --i){ // 0 is no jump
+        open_valencies.push_back(open_valencies.front());
+        open_valencies.pop_front();
+      }
+      jumps.pop_front();
+    }
+
+    // add node to spiral
+    open_valencies.push_back(make_pair(k,spiral_string[k]));
+
+    // connect k to k-1
+    wg_connect_backward(edge_set, open_valencies);
+
+    // connect k to k-2, etc
+    wg_connect_forward(edge_set, open_valencies);
+
+    // do the remaining connect forwards
+    while(open_valencies.front().second==0){
+      open_valencies.pop_front();
+      wg_connect_forward(edge_set, open_valencies);
+    }
+    // do the remaining connect backwards //not neat but the most simple way to emulate 'while second_last->second==0) ...'
+    while(true){
+      list<pair<int,int> >::iterator second_last(open_valencies.end());
+      --second_last;
+      --second_last;
+      if(second_last->second==0){
+        open_valencies.erase(second_last);
+        wg_connect_backward(edge_set, open_valencies);
+      } else break;
+      
+    }
+//    pdp(open_valencies);
+
+    if (open_valencies.back().second == 0){//the current atom is saturated (which may only happen for the last one)
+      cout << "Cage closed but faces left (or otherwise invalid spiral)" << endl;
+      abort();
+    }
+   
+  }//iterate over atoms
+
+  // make sure we left the spiral in a sane state
+  // open_valencies must be either spiral.back() times '1' at this stage
+  if(open_valencies.size() != spiral_string.back()){
+    cout << "Cage not closed but no faces left (or otherwise invalid spiral), wrong number of faces left" << endl;
+    abort();
+  }
+  for(list<pair<int,int> >::iterator it = open_valencies.begin(); it!=open_valencies.end(); ++it){
+    if(it->second!=1){
+      cout << "Cage not closed but no faces left (or otherwise invalid spiral), more than one valency left for at least one face" << endl;
+    abort();
+    }
+  }
+
+  // add last node to spiral // the name of the last node is N -1 (because it's the last one)
+  open_valencies.push_back(make_pair(N-1,spiral_string[N-1]));
+
+  for(int i=0; i<spiral_string.back(); ++i){
+    wg_connect_forward(edge_set, open_valencies);
+    open_valencies.pop_front();
+  }
+
+  *this = Triangulation(PlanarGraph(edge_set));
+}
+
