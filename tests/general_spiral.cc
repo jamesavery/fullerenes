5d21826d9295d2e72af3a23a9fa3d67b2d8cbe18
Lukas <l.wirz@massey.ac.nz>
2014-02-13 12:31:51 +1300

    make the general spiral test print full strings rather than only the abbreviation

--- tests/general_spiral.cc
+++ tests/general_spiral.cc
@@ -16,10 +16,14 @@ int main(int ac, char **av)
 //  int n = 28;
 //  int pentagon_indices_array[] = {0, 1, 2, 4, 7, 8, 10, 11, 12, 13, 14, 15};
 //jump, 380
-//  int n = 380;
+  int n = 380;
 //  int pentagon_indices_array[] = {0, 1, 2, 136, 137, 146, 147, 156, 157, 165, 172, 179};
+  int pentagon_indices_array[] = {44, 69, 70, 81, 82, 109, 118, 119, 143, 183, 184, 191};
 //  int jumps_array[][2] = {{165,2}, {172,3}, {179,3}};
+  int jumps_array[][2] = {{109,2}};
 //  int n_jumps = 3;
+  int n_jumps = 1;
+  //0,1,2
 //jump, 440
 //  int n = 440;
 //  int pentagon_indices_array[] = {159, 160, 161, 174, 175, 176, 189, 190, 191, 195, 204, 212};
@@ -27,11 +31,11 @@ int main(int ac, char **av)
 //  int n_jumps = 4;
 //  //0, 1, 9
 //jump, 672
-  int n = 672;
-  int pentagon_indices_array[] = {140, 142, 144, 161, 163, 165, 225, 242, 328, 330, 333, 335};
-  int jumps_array[][2] = {{225,5}, {242,6}};
-  int n_jumps = 2;
-  //0, 115, 151
+//  int n = 672;
+//  int pentagon_indices_array[] = {140, 142, 144, 161, 163, 165, 225, 242, 328, 330, 333, 335};
+//  int jumps_array[][2] = {{225,5}, {242,6}};
+//  int n_jumps = 2;
+//  //0, 115, 151
 
   vector<int> pentagon_indices_input(12);
   vector<int> pentagon_indices_output;
@@ -71,19 +75,21 @@ int main(int ac, char **av)
 
   jumps.clear();
   pentagon_indices_output.clear();
-  fg.get_general_spiral_from_fg(0,115,151, pentagon_indices_output, jumps);  
+//  fg.get_general_spiral_from_fg(0,1,2, pentagon_indices_output, jumps);  
+  fg.get_canonical_general_spiral_from_fg(pentagon_indices_output, jumps);  
 
-  cout << "Output spiral indices: ";
+  cout << "Output spiral: ";
+  vector<int> out_spiral(n/2+2,6);
   for (int i=0; i<12; ++i){
-    cout <<  pentagon_indices_output[i] << ", ";
+    out_spiral[pentagon_indices_output[i]] = 5;
   }
-  cout << endl;
+  cout << out_spiral << endl;
+  
+  cout << "Output spiral indices: ";
+  cout << pentagon_indices_output << endl;
   
   cout << "Jumps: ";
-  for (list<pair<node_t,int> >::iterator it=jumps.begin(); it!=jumps.end(); ++it){
-    cout <<  it->first << ": " << it->second << ", ";
-  }
-  cout << endl;
+  cout << jumps << endl;
 
   return 0;
 }
