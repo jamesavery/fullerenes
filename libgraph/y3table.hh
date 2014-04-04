556752ceba51c446df6b707e82a728eea148639f
James Avery <avery@diku.dk>
2014-04-05 01:39:40 +1300

    First go at spherical harmonic decomposition, using only vertices for integrals.

---
+++ libgraph/y3table.hh
@@ -0,0 +1,10 @@
+#ifndef Y3TABLE_HH
+# define Y3TABLE_HH
+
+#include <inttypes.h>
+namespace Y3Table {
+const uint16_t Nvalues = 1089, Nindices = 169;
+extern const uint16_t indices[169], keys[1089];
+extern const double values[1089];
+}
+#endif
