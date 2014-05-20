b09ac5a69263c575dcd40e2ab1c3ae47f6a067e3
James Avery <avery@diku.dk>
2014-05-20 19:33:11 +0200

    Generating RSPI

--- apps/db-rspi.cc
+++ apps/db-rspi.cc
@@ -14,17 +14,25 @@ int main(int ac, char **av)
   int N            = ac>=2? strtol(av[1],0,0) : 20;
   int chunk_number = ac>=3? strtol(av[2],0,0) : 1;
   int chunk_index  = ac>=4? strtol(av[3],0,0) : 0;
+  int ipr              = ac>=5? strtol(av[4],0,0) : 0;
+  int only_nontrivial  = ac>=6? strtol(av[5],0,0) : 0;
 
-  BuckyGen::buckygen_queue Q = BuckyGen::start(N,0,chunk_index,chunk_number);
+  BuckyGen::buckygen_queue Q = BuckyGen::start(N,ipr,only_nontrivial,chunk_index,chunk_number);
   
   Triangulation G;
   PlanarGraph dual;
   int i=0;
 
-  FILE *output = fopen(("output/c"+to_string(N)+".rspi"
-			+(chunk_number==1?"":("."+to_string(chunk_number)+"-"+to_string(chunk_index)))).c_str(),"wb");
+  string outputfile = ("output/c"+to_string(N)+(ipr?"-IPR":"")+(only_nontrivial?"-nontrivial":"")+".rspi"
+		       +(chunk_number==1?"":("."+to_string(chunk_number)+"-"+to_string(chunk_index))));
+  FILE *output = fopen(outputfile.c_str(),"wb");
 
-  if(!output) abort();
+  if(!output){
+	 perror(outputfile.c_str());
+	 abort();
+  }
+
+  cerr << "Writing to " << outputfile << "\n";
 
   while(BuckyGen::next_fullerene(Q,G)){
     uint8_t rspi[12];
@@ -33,7 +41,7 @@ int main(int ac, char **av)
     
     i++;
     G = Triangulation(G.neighbours,true);
-    bool spiral_OK = G.get_spiral(spiral,jumps,false);
+    bool spiral_OK = G.get_spiral(spiral,jumps,true,false);
     if(!spiral_OK){
       vector<int> rspi2(12);
       FullereneGraph F(G.dual_graph());
@@ -47,7 +55,7 @@ int main(int ac, char **av)
 
     get_rspi(spiral,rspi);
 
-    if(i % 100 == 0) cout << "Isomer " << i << ": " << vector<int>(rspi,rspi+12) << endl;
+    if(i % 1000 == 0) cout << "Isomer " << i << ": " << vector<int>(rspi,rspi+12) << endl;
 
     fwrite(rspi,12,1,output);
 
