74319ade666ec9c4ce1244ec572eeacf09d67f20
Lukas <l.wirz@massey.ac.nz>
2015-10-27 17:22:40 +1300

    fix spiral app, describe apps

--- apps/spiral.cc
+++ apps/spiral.cc
@@ -21,9 +21,8 @@ int main(int ac, char **av)
   cout << "Attempting to create graph from spiral indices " << rspi << endl;
  
   FullereneGraph g(N, rspi, jumps);
-  PlanarGraph  dg(g.dual_graph(6));
-
   g.layout2d = g.tutte_layout();
+  PlanarGraph  dg(g.dual_graph(6));
   dg.layout2d = dg.tutte_layout();
 
   cout << "g = " << g << ";\n";
@@ -37,22 +36,5 @@ int main(int ac, char **av)
   g_latex.close(); 
   dg_latex.close();
 
-  // Get and print distance matrix for all vertices and for pentagons
-  vector<int> D(dg.all_pairs_shortest_paths());
-  cout << "D = {\n";
-  for(int i=0;i<dg.N;i++) cout << "\t" << vector<unsigned int>(&D[i*dg.N],&D[(i+1)*dg.N]) << "}" << (i+1<dg.N?",\n":"\n");
-  cout << "};\n";
-  
-  vector<unsigned int> pentagons;
-  for(int i=0;i<dg.N;i++) if(dg.neighbours[i].size() == 5) pentagons.push_back(i);
-
-  cout << "D5 = {\n";
-  for(int i=0;i<12;i++){
-    cout << "\t{";
-    for(int j=0;j<12;j++) cout << D[i*dg.N+j] << (j+1<12?",":"");
-    cout << "}" << (i+1<12?",\n":"\n");
-  }
-  cout << "};\n";
-  
   return 0;
 }
