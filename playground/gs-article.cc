46c946584706a1e99ca05a467a267b8d98503ea9
Lukas N Wirz <l.n.wirz@kjemi.uio.no>
2016-12-23 12:32:58 +0100

    .

--- playground/gs-article.cc
+++ playground/gs-article.cc
@@ -1,50 +1,56 @@
 
-// fullerene_graph_ptr goldberg_coxeter_(const fullerene_graph_ptr *g, const int *k, const int *l)
-// {
-//   FullereneGraph fg(**g);
-//   fg.layout2d = fg.tutte_layout(); // FIXME remove, and pass argument to GCtransform?
-//   fg.layout_is_spherical = false;
-//   return new FullereneGraph(fg.GCtransform(*k,*l));
-// }
-
-
 #include "libgraph/fullerenegraph.hh"
-#include "libgraph/polyhedron.hh"
-#include "libgraph/isomerdb.hh"
 #include "libgraph/triangulation.hh"
+#include "libgraph/polyhedron.hh"
 
 
 int main(int ac, char **av)
 {
-  int k = strtol(av[1],0,0);
-  int l = strtol(av[2],0,0);
-  cout << k << ", " << l << endl;
+  int N = strtol(av[1],0,0);
+  int k = strtol(av[2],0,0);
+  int l = strtol(av[3],0,0);
+  cout << N << ", " << k << ", " << l << endl;
 
-//  const int N=380;
-//  vector<int> input_rspi({44,69,70,81,82,109,118,119,143,183,184,191});
-//  Triangulation::jumplist_t input_jumps({{109,2}});
+  FullereneGraph g;
+  switch(N){
+    case 380:{
+      vector<int> input_rspi({44,69,70,81,82,109,118,119,143,183,184,191});
+      Triangulation::jumplist_t input_jumps({{109,2}});
+      g=FullereneGraph(N,input_rspi,input_jumps);
+      break;
+    }
+  
+    case 384:{
+      vector<int> input_rspi({28,29,30,48,144,145,169,170,189,190,191,193});
+      Triangulation::jumplist_t input_jumps({{48,1}});
+      g=FullereneGraph(N,input_rspi,input_jumps);
+      break;
+    }  
 
-//  const int N=384;
-//  vector<int> input_rspi({28,29,30,48,144,145,169,170,189,190,191,193});
-//  Triangulation::jumplist_t input_jumps({{48,1}});
+    case 440:{
+      vector<int> input_rspi({38,39,40,61,169,170,196,197,217,218,219,221});
+      Triangulation::jumplist_t input_jumps({{61,1}});
+      g=FullereneGraph(N,input_rspi,input_jumps);
+      break;
+    }
+  
+    case 672:{
+      vector<int> input_rspi({50,52,108,110,219,251,287,301,303,305,319,337});
+      Triangulation::jumplist_t input_jumps({{141,1}});
+      g=FullereneGraph(N,input_rspi,input_jumps);
+      break;
+    }
 
-//  const int N=440;
-//  vector<int> input_rspi({38,39,40,61,169,170,196,197,217,218,219,221});
-//  Triangulation::jumplist_t input_jumps({{61,1}});
+    default:
+      return 1;
+  }
 
-  const int N=672;
-  vector<int> input_rspi({50,52,108,110,219,251,287,301,303,305,319,337});
-  Triangulation::jumplist_t input_jumps({{141,1}});
-
-
-  FullereneGraph g(N,input_rspi,input_jumps);
   cout << "g = " << g << ";\n";
 
   g.layout2d = g.tutte_layout();
   g.layout_is_spherical = false;
 cout << "Tutte done" << endl;
 
-  //const int k=3,l=5;
   FullereneGraph gkl(g.GCtransform(k,l));
 cout << "gc done " << endl;
 
@@ -56,8 +62,13 @@ cout << "Tutte done" << endl;
   Triangulation::jumplist_t jumps;
   gkl.get_rspi_from_fg(rspi, jumps, true, true);
 
-  cout << "spiral   = " << rspi << ";\n"
-  	 << "jumplist = " << jumps << ";\n";
+ 
+  ofstream output(("spiral-"+to_string(N)+"-"+to_string(k)+"-"+to_string(l)).c_str());
+  for (int i=0;i<12;i++)rspi[i]++;
+  for (auto it=jumps.begin();it!=jumps.end();it++) it->first++;
+  output << "jumplist = " << jumps << ";\n"
+         << "spiral   = " << rspi << ";\n";
+  output.close() ;
   
   return 0;
 }
