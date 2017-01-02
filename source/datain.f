d010c9a945cbc71ba6dd701aed511b07f6c62f0e
Peter Schwerdtfeger <p.a.schwerdtfeger@massey.ac.nz>
2017-01-02 14:48:21 +1300

    Changes to datain

--- source/datain.f
+++ source/datain.f
@@ -112,9 +112,6 @@ C Integers
       ndbconvert=0 ! Flag for conversion of database
       nosort=0  !  Flag for sorting cartesian coordinates
       ispsearch=1 ! Search through all spirals without jumps, if unsuccessful do general spiral algorithm
-      ispsearch=2 ! Search through spirals without jumps, but stop in (55), (56) or (66) batch if found, if unsuccessful do general spiral algorithm
-      ispsearch=3 ! Go directly to general spiral algorithm
-      ispsearch=4 ! Same as ispsearch=1 but only do pentagon starts
       ihamstore=0 ! Flag for storing all Hamiltonian cycles
       ihamstats=0 ! Flag for Hamiltonian cycle statistics
       nhamcyc=0 ! Flag for reading Hamiltonian cycle for 2D graph
