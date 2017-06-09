094dc62e87d8d70829a6ba7b02daaa83d92f5f98
James Emil Avery <avery@diku.dk>
2017-06-09 13:23:37 +0200

    file-to-pov updated (used old fns)

--- apps/file-to-pov.cc
+++ apps/file-to-pov.cc
@@ -8,8 +8,7 @@ int main(int ac, char **av)
   Polyhedron P(av[1]);
   
   P.move_to_origin();
-  matrix3d If(P.inertial_frame());
-  P.points = If*P.points;
+  P.align_with_axes();
 
   cout << P.to_povray() << endl;
   
