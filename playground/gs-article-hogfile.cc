e7fbcffde11e9a595fe676ef7f195365c47ef9bc
James Emil Avery <avery@diku.dk>
2017-07-24 02:00:21 +0200

    Benchmark for HoG plancode database (for fullerenes with no pentagon spiral)

--- playground/gs-article-hogfile.cc
+++ playground/gs-article-hogfile.cc
@@ -3,8 +3,10 @@
 #include "libgraph/triangulation.hh"
 
 #include <iostream>
-#include <chrono>
+#include <chrono>		// For wall-time
+#include <ctime>		// For CPU-time
 
+using namespace std;
 typedef Triangulation::jumplist_t jumplist_t;
 
 vector<int> rspi_from_fullerene_spiral(const vector<int>& spiral)
@@ -41,20 +43,27 @@ int main(int ac, char **av)
   //  cout << "graphs = " << graphs << ";\n";
   //  cout << "duals = " << duals << ";\n";
   //  return 0;
-  vector<int> spiral(duals[0].N);
+  int N = graphs[0].N, NF = duals[0].N;
+  vector<int> spiral(NF);
   jumplist_t jumps;
 
   vector<jumplist_t> all_jumps(duals.size());
 
-  auto start = chrono::steady_clock::now();
+  auto wall_start = chrono::steady_clock::now();
+  auto cpu_start  = clock();
   for(int i=0;i<duals.size();i++){
     duals[i].get_spiral(spiral,jumps,true,true,true,true);
     all_jumps[i] = jumps;
   }
-  auto end = chrono::steady_clock::now();
+  auto cpu_end = clock();
+  auto wall_end = chrono::steady_clock::now();
+  double walltime = chrono::duration<double,std::ratio<1,1>>(wall_end-wall_start).count();
+  double cputime  = (cpu_end-cpu_start) * 1.0 / CLOCKS_PER_SEC;
   cerr << "Computed all spirals.\n";
   cout << "Nisomers = " << duals.size() << ";\n";
-  cout << "timing   = " << chrono::duration<double,std::ratio<1,1>>(end-start).count() << ";\n";
+  cout << "walltime = " << walltime << ";\n";
+  cout << "cputime  = " << cputime << ";\n";
+  cout << "rate     = " << cputime/(duals.size()*N) << ";\n";
   cout << "alljumps = " << all_jumps << ";\n";
   
   return 0;
