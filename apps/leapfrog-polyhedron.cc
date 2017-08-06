3e208325bb50414781b895718bab89491e568eda
James Avery <avery@diku.dk>
2017-08-06 02:22:16 +0200

    General leapfrog test: Read non-cubic / non-triangulation from HoG

--- apps/leapfrog-polyhedron.cc
+++ apps/leapfrog-polyhedron.cc
@@ -4,7 +4,7 @@
 
 typedef Triangulation::jumplist_t jumplist_t;
 
-bool grspi_from_args(int ac, char **av, int &N, jumplist_t &jumps, vector<int> &RSPI)
+bool grspi_from_args(int ac, char **av, int &N, vector<int> &RSPI, jumplist_t &jumps)
 {
   if(ac < 14) return false;
   
@@ -19,38 +19,56 @@ bool grspi_from_args(int ac, char **av, int &N, jumplist_t &jumps, vector<int> &
   return true;
 }
 
-
-int main(int ac, char **av)
+Graph planargraph_from_args(int ac, char **av)
 {
   int N;
   jumplist_t jumps;
   vector<int> RSPI(12);
 
-  grspi_from_args(ac,av,N,jumps,RSPI);
-  
-  FullereneDual dF(N,RSPI,jumps);
-  PlanarGraph    F(dF.dual_graph());
-  F.layout2d = F.tutte_layout();
-  
-  cout << "dF = " << dF << ";\n"
-       << "F  = " << F  << ";\n";
+  if(ac >= 14){
+    grspi_from_args(ac,av,N,RSPI,jumps);
+    return FullereneDual(N,RSPI,jumps).dual_graph();
+  } else if (ac >= 3) { // Read from HoG planarcode file
+    FILE *file = fopen(av[1],"rb");
+    int index  = strtol(av[2],0,0);
+    assert(file != 0);
 
-  assert(dF.is_consistently_oriented());
-  assert(F.is_consistently_oriented());
-  
-  Triangulation G = F.leapfrog_dual();
+    vector<Graph> graphs = PlanarGraph::read_hog_planarcodes(file);
+    return graphs[index];
+  } else {
+    fprintf(stderr,"Syntax: %s [<HoG file> <index> | <N> <general spiral>]\n",av[0]);
+    abort();
+  }
+}
 
+int main(int ac, char **av)
+{
+
+  PlanarGraph  g(planargraph_from_args(ac,av));
+  g.layout2d = g.tutte_layout();
+  
+  cout << "g  = " << g  << ";\n";
+
+  assert(g.is_consistently_oriented());
+  
+  Triangulation dG = g.leapfrog_dual();
+
+  assert(dG.is_consistently_oriented());  
+  cout << "dG = " << dG << ";\n";
+
+  PlanarGraph G = dG.dual_graph();
   assert(G.is_consistently_oriented());  
   cout << "G = " << G << ";\n";
-
+   
   {
     vector<int> spiral;
     jumplist_t  jumps;
-    G.get_spiral(spiral,jumps);
+    dG.get_spiral(spiral,jumps);
 
     cout << "LFspiral = " << spiral << ";\n"
 	 << "LFjumps  = " << jumps  << ";\n";
   }
+
   
   return 0;
 }
