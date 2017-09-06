cc73b89473d505d421d4a489e71a22c1df29741b
James Emil Avery <avery@diku.dk>
2017-09-06 19:36:16 +0200

    Start on implementation of (general) general spirals.
    I.e., including D/LF-modifiers, so that any polyhedral graph can be encoded/decoded.

---
+++ libgraph/spiral.cc
@@ -0,0 +1,86 @@
+#include "spiral.cc"
+
+bool operator<(const general_spiral &s)
+{
+  return jumps.size() < s.jumps.size() ||
+	(jumps.size() == s.jumps.size() && jumps < s.jumps) ||
+        (jumps == s.jumps && spiral < s.spiral);
+}
+
+
+ostream &operator<<(ostream &s, const general_spiral &GS)
+{
+  return s << make_pair(GS.jumps,GS.spiral); 
+}
+
+
+spiral_nomenclature::spiral_nomenclature(const PlanarGraph &g, const string& atom="",
+					 bool compatibility=false) : atom(atom), graph(g),cs(compatibility) {
+  if(g.is_triangulation()){
+    graph_type     = TRIANGULATION;
+    is_a_fullerene = g.dual_graph().is_a_fullerene();
+    triangulation  = g;
+  } else if(g.is_cubic()){
+    graph_type     = CUBIC;
+    triangulation  = g.dual_graph();
+    is_a_fullerene = g.is_a_fullerene();
+  } else {
+    graph_type     = GENERAL;
+    triangulation  = g.leapfrog_dual();
+    is_a_fullerene = false;
+  }
+
+  permutation.resize(triangulation.N);
+  bool spiral_success = triangulation.get_spiral(GS.spiral,GS.jumps,!cs);
+  assert(spiral_success);
+
+  // Find spiral order with respect to triangulation vertices
+  permutation.resize(triangulation.N);
+  spiral_success = triangulation.get_spiral(GS.spiral,GS.jumps,permutation,!cs);
+}
+
+template <typename T> vector<T> read_list
+spiral_nomenclature::spiral_nomenclature(const string& name)
+{
+  // Grammar for nomenclature as described in spiral paper:
+  // 
+  // MOLECULE_NAME   ::= (POINTGROUP '-')? POLYHEDRON_NAME '-' FORMULA '-' SUFFIX
+  // POLYHEDRON_NAME ::= '[' METHOD_SPEC? (JUMP+';')? SPIRAL ']'
+  //                 |   '[' METHOD_SPEC? (JUMP+';')? PI     ']'
+  //                 |   '[' METHOD_SPEC? (JUMP+';')? FI     "]-(" FACE_SIZES ")" BASE_FACE
+  //
+  // METHOD_SPEC     ::= ("D"|"LF"|"CS"|"GS")+ ':' 
+  // SUFFIX          ::= 'cage' | 'fullerene' | 'fulleroid'
+  // SPIRAL          ::= NUMBER_LIST
+  // PI              ::= NUMBER_LIST
+  // FI              ::= NUMBER_LISTS
+  // NUMBER_LIST     ::= number (',' number)*
+  // NUMBER_LISTS    ::= NUMBER_LIST (';' NUMBER_LIST)*
+  // FORMULA         ::= string
+  // POINTGROUP      ::= string
+
+  // Simplified grammar which accepts the above + some more:
+  //
+  // SEPARATOR  ::= " []-"
+  // POLYHEDRON_NAME ::= '[' METHOD_SPEC? NUMBER_LISTS ']'
+  //                 |   '[' METHOD_SPEC? NUMBER_LISTS "]-(" NUMBER_LIST ")" number
+  //
+  // FI              ::= NUMBER_LIST (';' NUMBER_LIST)*
+  // FORMULA         ::= string
+  // POINTGROUP      ::= string
+  
+  // The grammar is so simple that we'll just brutally parse by hand.
+
+  for(int i=0;i<name.size();i++){
+  }
+
+}
+
+friend ostream& operator<<(ostream &s, const spiral_nomenclature &n){
+  string graph_type_string[3] = {"","D,","LF,"};
+  s << "["<<graph_type_string[n.graph_type] <<(n.compatibility?"CS":"GS") << ": "
+    << (n.GS.jumps.empty()? "": (jumps_to_string(n.GS.jumps)+"; "))
+    << (n.is_a_fullerene? spiral_to_rspi_string(n.GS.spiral) : spiral_to_string(n.GS.spiral))
+    << "]-" <<n.atom<< n.graph.N <<"-" << (n.is_a_fullerene? "fullerene" : "cage");
+  return s;
+}
---
+++ libgraph/spiral.hh
@@ -0,0 +1,52 @@
+#pragma once
+//TODO: The plan is to gather the spiral stuff here, so it's not scattered around the place.
+
+typedef pair<int,int>  jump_t;
+typedef vector<jump_t> jumplist_t;
+
+// This general spiral type can represent any polyhedral graph.
+struct general_spiral {
+  typedef enum { CUBIC, TRIANGULATION, GENERAL } spiral_type;
+
+  vector<int> spiral;  
+  jumplist_t  jumps;
+  spiral_type type;
+
+  general_spiral(const vector<int>& spiral,
+		 const jumplist_t& jumps = jumplist_t(),
+		 const polyhedron_type& type) : spiral(spiral),
+						jumps(jumps),
+						type(type) {}
+
+  general_spiral(const PlanarGraph &g, bool compatibility=false);
+  general_spiral(const string& name);
+  
+  bool operator<(const general_spiral &s);
+  friend ostream &operator<<(ostream &s, const general_spiral &GS);
+};
+
+
+struct spiral_nomenclature {
+  bool is_a_fullerene;
+  string atom;
+  string point_group;
+  
+  PlanarGraph   graph;
+  Triangulation triangulation;
+  bool compatibility;
+
+  general_spiral GS;
+  Permutation permutation;
+
+  spiral_nomenclature(const PlanarGraph &g, const string& atom="", bool compatibility=false);
+  spiral_nomenclature(const string& name);
+  
+  friend ostream& operator<<(ostream &s, const spiral_nomenclature &n);
+
+
+  
+  
+};
+
+
+#endif
