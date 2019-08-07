c96b84c05a9900c5767c1bf20de16bc3301ba3ae
James Emil Avery <avery@diku.dk>
2019-08-07 13:52:50 +0200

    Generate polyhedron from spiral code

---
+++ programs/spiral-to-polyhedron.cc
@@ -0,0 +1,25 @@
+#include <string>
+
+#include "fullerenes/triangulation.hh"
+#include "fullerenes/spiral.hh"
+#include "fullerenes/polyhedron.hh"
+
+using namespace std;
+
+int main(int ac, char **av)
+{
+  string spiral_name = av[1];
+  spiral_nomenclature fsn(spiral_name);
+  PlanarGraph g(fsn);
+
+  g.layout2d = g.tutte_layout();
+  Polyhedron P(g,g.zero_order_geometry());
+  P.optimize();
+  
+  cout << "spiralcode = \"" << spiral_name << "\";\n"
+       << "fullname   = " << fsn << ";\n"
+       << "g = " << g << ";\n"
+       << "P = " << P << ";\n";
+    
+  return 0;
+}
