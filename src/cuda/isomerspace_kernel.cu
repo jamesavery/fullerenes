be748ab90b8b0fcf1235339b7717ff4ae9111788
Jonaslacour <jonaslacour1@gmail.com>
2022-02-27 12:20:13 +0100

    Isomerspace kernel is responsible for larger fraction of construction / destruction TODO: generify main kernel call as well

--- src/cuda/isomerspace_kernel.cu
+++ src/cuda/isomerspace_kernel.cu
@@ -4,8 +4,7 @@
 #include "cuda.h"
 #include "cuda_runtime_api.h"
 
-template <typename T>
-size_t IsomerspaceKernel<T>::get_batch_capacity(){
+size_t IsomerspaceKernel::get_batch_capacity(){
     cudaDeviceProp properties;
     int device_count = 0;
     int total_capacity = 0;
@@ -24,8 +23,7 @@ size_t IsomerspaceKernel<T>::get_batch_capacity(){
     return (size_t)total_capacity;
 }
 
-template <typename T>
-IsomerspaceKernel<T>::IsomerspaceKernel(const size_t N, void* kernel){
+IsomerspaceKernel::IsomerspaceKernel(const size_t N, void* kernel){
     cudaGetDeviceCount(&this->device_count);
     kernel_pointer          = kernel;
     global_reduction_arrays = std::vector<device_real_t*>(device_count);
@@ -35,20 +33,27 @@ IsomerspaceKernel<T>::IsomerspaceKernel(const size_t N, void* kernel){
     batch_capacity          = get_batch_capacity();    
     index_queue             = std::vector<std::queue<int>>(device_count);
 
-    for (size_t i = 0; i < device_count; i++)
-    {
-        cudaMalloc(&global_reduction_arrays[i], sizeof(device_real_t)*N*device_capacities[i]);
-        batch_sizes[i] = 0;
-        for (size_t j = 0; j < device_capacities[i]; j++)   index_queue[i].push(j);
-        
-    }
-}
+    d_batch = std::vector<IsomerBatch>(device_count);
+    h_batch = std::vector<IsomerBatch>(device_count);
 
-template <typename T>
-IsomerspaceKernel<T>::~IsomerspaceKernel(){
     for (size_t i = 0; i < device_count; i++)
     {
         cudaSetDevice(i);
-        cudaFree(global_reduction_arrays[i]);
+        GPUDataStruct::allocate(d_batch[i]         , N, device_capacities[i], DEVICE_BUFFER);
+        GPUDataStruct::allocate(h_batch[i]         , N, device_capacities[i], HOST_BUFFER);
+
+        cudaMalloc(&global_reduction_arrays[i], sizeof(device_real_t)*N*device_capacities[i]);
+        batch_sizes[i] = 0;
+        for (size_t j = 0; j < device_capacities[i]; j++) index_queue[i].push(j);
+        for (size_t j = 0; j < device_capacities[i]; j++) h_batch[i].statuses[j] = EMPTY;
+    }
+}
+
+IsomerspaceKernel::~IsomerspaceKernel(){
+    for (size_t i = 0; i < device_count; i++)
+    {
+        cudaSetDevice(i);
+        GPUDataStruct::free(h_batch[i]);
+        cudaDeviceReset();
     }
 }
