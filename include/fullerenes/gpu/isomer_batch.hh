8625cf5fb7e73f9c6019f80d85352fb862c67159
Jonaslacour <jonaslacour1@gmail.com>
2022-03-01 18:11:56 +0100

    changed allocation size of memory, for future use of direct copying from flat 3xN arrays and performing type conversion on device

--- include/fullerenes/gpu/isomer_batch.hh
+++ include/fullerenes/gpu/isomer_batch.hh
@@ -16,6 +16,6 @@ struct IsomerBatch : GPUDataStruct
     size_t* iterations;
 
     IsomerBatch(){
-      pointers =   {{"neighbours",(void**)&neighbours, sizeof(device_node_t)*3, true}, {"X", (void**)&X, sizeof(device_real_t)*3, true}, {"xys", (void**)&xys, sizeof(device_real_t)*2, true}, {"statuses", (void**)&statuses, sizeof(IsomerStatus), false}, {"IDs", (void**)&IDs, sizeof(size_t), false}, {"iterations", (void**)&iterations, sizeof(size_t), false}};
+      pointers =   {{"neighbours",(void**)&neighbours, sizeof(node_t)*3, true}, {"X", (void**)&X, sizeof(double)*3, true}, {"xys", (void**)&xys, sizeof(double)*2, true}, {"statuses", (void**)&statuses, sizeof(IsomerStatus), false}, {"IDs", (void**)&IDs, sizeof(size_t), false}, {"iterations", (void**)&iterations, sizeof(size_t), false}};
     }
 };
