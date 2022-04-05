0d7f4acb6685392331fa9e15871e910df7d4940d
Jonaslacour <jonaslacour1@gmail.com>
2022-04-05 17:17:03 +0200

    Copy function needs to accept a stream argument for asynchronous execution

--- include/fullerenes/gpu/gpudatastruct.hh
+++ include/fullerenes/gpu/gpudatastruct.hh
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <vector>
 #include <string>
+#include <cuda_runtime.h>
 
 enum BufferType   {HOST_BUFFER, DEVICE_BUFFER};
 
@@ -15,5 +16,5 @@ struct GPUDataStruct{
     std::vector<std::tuple<std::string,void**,size_t,bool>> pointers;
     static void allocate(GPUDataStruct& G,const size_t N,const size_t batch_size, const BufferType buffer_type);
     static void free(GPUDataStruct& G);
-    static void copy(GPUDataStruct& destination, const GPUDataStruct& source);
+    static void copy(GPUDataStruct& destination, const GPUDataStruct& source, cudaStream_t stream = NULL);
 };  
