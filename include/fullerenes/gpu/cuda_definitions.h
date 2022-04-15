55257283896a1d09635c183d75bd933446d6c9fc
Jonaslacour <jonaslacour1@gmail.com>
2022-04-15 19:29:18 +0200

    more definitions and typedefs

--- include/fullerenes/gpu/cuda_definitions.h
+++ include/fullerenes/gpu/cuda_definitions.h
@@ -1,9 +1,14 @@
 #ifndef FULLERENE_CUDA_DEFINES
 #define FULLERENE_CUDA_DEFINES
 
+#include "inttypes.h"
+
+#define BLOCK_SYNC cg::sync(cg::this_thread_block());
+#define GRID_SYNC cg::sync(cg::this_grid());
 #define GPU_REAL float       
 #define GPU_REAL3 float3
 #define GPU_REAL2 float2
+#define GPU_NODE uint16_t
 #define GPU_NODE2 ushort2
 #define GPU_NODE3 ushort3
 #define sin sinf		// change to sin for double precision, sinl for long double
@@ -15,6 +20,7 @@
 #define USE_MAX_NORM 0
 #define REDUCTION_METHOD 0
 #define LINESEARCH_METHOD GSS
-
+typedef GPU_REAL device_real_t;
+typedef GPU_NODE device_node_t;
 
 #endif
