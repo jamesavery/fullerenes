23db5f089737fd359ef4f653cf4e1c5ec4e8052d
Jonaslacour <jonaslacour1@gmail.com>
2022-04-16 13:11:14 +0200

    Added: Interface reflects the implementation

--- include/fullerenes/gpu/misc_cuda.cuh
+++ include/fullerenes/gpu/misc_cuda.cuh
@@ -1,5 +1,9 @@
+#ifndef MISCEllANEOUS_CUDA_FUN
+#define MISCEllANEOUS_CUDA_FUN
+
 #include "cuda_definitions.h"
 #include "cuda_runtime_api.h"
+#include "string"
 
 //Sets the memory to 0. This exists because L1 cache is reused for different purposes, thus pointers alias.
 //Algorithms that depend on cache being 0 initialized to avoid bound checking must be prepared with a call to this function.
@@ -12,3 +16,17 @@ __device__ void swap_reals(T& real1, T& real2);
 
 //Atomically updates the data at the location *data, in order of threadIdx.x (floating point math is not associative)
 __device__ void ordered_atomic_add(GPU_REAL* data, const GPU_REAL element);
+
+//Initializes device memory with the provided value.
+
+//Prints the last cuda error prepended by a custom string.
+void printLastCudaError(std::string message = "");
+
+//Checks if the dimensions passed are non-zero.
+cudaError_t safeCudaKernelCall(const void* func, const dim3 gridDim, const dim3 blockDim, void** args, const size_t sharedMem, const cudaStream_t stream = NULL);
+
+//Initialization function for device arrays.
+template <typename T>
+cudaError_t fill_cu_array(T* cu_array, size_t size, T fill_value);
+
+#endif
