ea3c22039746a86bf7a6dad6a9227fa20f944f79
Jonaslacour <jonaslacour1@gmail.com>
2022-05-03 04:36:05 +0200

    Added: some comments and new LaunchCtx and LaunchPolicy parameters as well as new performance metric getter functions

--- include/fullerenes/gpu/kernels.hh
+++ include/fullerenes/gpu/kernels.hh
@@ -1,14 +1,25 @@
 #include "isomerspace_kernel.hh"
+#include "cuda_execution.hh"
 namespace gpu_kernels{
     namespace isomerspace_forcefield{
-        cudaError_t optimize_batch(IsomerBatch& B, const size_t iterations, const size_t max_iterations, const cudaStream_t stream = NULL);
+        int optimal_batch_size(const int N, const int device_id = 0);
+        //Returns total time spent in kernel in milliseconds. First call is not timed as that would require synchronous execution.
+        float time_spent(); 
+        //Uses forcefield optimization to relax the positions of atoms in all isomers of a batch.
+        cudaError_t optimize_batch(IsomerBatch& B, const size_t iterations, const size_t max_iterations, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
     }
 
     namespace isomerspace_X0{
-        cudaError_t zero_order_geometry(IsomerBatch& B, const device_real_t scalerad, const cudaStream_t stream = NULL);
+        //Returns total time spent in kernel in milliseconds. First call is not timed as that would require synchronous execution.
+        float time_spent(); 
+        //Uses spherical projection and 'wrapping a sphere' technique to generate starting coordinates in 3D space for a batch of fullerenes.
+        cudaError_t zero_order_geometry(IsomerBatch& B, const device_real_t scalerad, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
     }
 
     namespace isomerspace_tutte{
-        cudaError_t tutte_layout(IsomerBatch& B, const size_t max_iterations = 1000, const cudaStream_t = NULL);
+        //Returns total time spent in kernel in milliseconds. First call is not timed as that would require synchronous execution.
+        float time_spent(); 
+        //Computes tutte embedding for an entire batch of fullernes.
+        cudaError_t tutte_layout(IsomerBatch& B, const size_t max_iterations = 1000, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
     }
 }
