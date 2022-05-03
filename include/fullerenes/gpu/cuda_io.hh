5326caa268b2efc3e518373d34d3f0ea91aa98a7
Jonaslacour <jonaslacour1@gmail.com>
2022-05-03 04:33:42 +0200

    Changed: moved reset_convergence_statuses into cuda_io namespace to be acessible from outside cuda source code, also gave all cuda_io functions LaunchCtx and LaunchPolicy arguments

--- include/fullerenes/gpu/cuda_io.hh
+++ include/fullerenes/gpu/cuda_io.hh
@@ -1,8 +1,10 @@
 #include <cuda_runtime.h>
 #include "isomerspace_kernel.hh"
+#include "cuda_execution.hh"
 
 namespace cuda_io{
     cudaError_t output_to_queue(std::queue<std::pair<Polyhedron, size_t>>& queue, IsomerBatch& batch, const bool copy_2d_layout = true);
-    cudaError_t copy(IsomerBatch& target, const IsomerBatch& source);
-    cudaError_t resize(IsomerBatch& batch, size_t new_capacity, const cudaStream_t stream = NULL);
+    cudaError_t copy(IsomerBatch& target, const IsomerBatch& source, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC, const std::pair<int,int>& lhs_range = {-1,-1}, const std::pair<int,int>& rhs_range = {-1,-1});
+    cudaError_t resize(IsomerBatch& batch, size_t new_capacity, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC, int front = -1, int back = -1);
+    cudaError_t reset_convergence_statuses(IsomerBatch& batch, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
 }
