25adb5e7cc614bfb515f511c316a5b5c9f2d15c8
Jonaslacour <jonaslacour1@gmail.com>
2022-05-04 23:41:12 +0200

    Added: some comments and explaination to how the BatchQueue works

--- src/cuda/batch_queue.cu
+++ src/cuda/batch_queue.cu
@@ -18,6 +18,7 @@ namespace cuda_io{
 namespace cuda_io{
 #include "print_functions.cu"
 
+//Casting functions to convert Graph objects and its' derivatives to IsomerBatch storage i.e flat memory representation.
 static auto casting_coord3d = [](coord3d in){return device_coord3d{static_cast<device_real_t>(in[0]), static_cast<device_real_t>(in[1]), static_cast<device_real_t>(in[2])};};
 static auto casting_node3   = [](std::vector<node_t> in){return device_node3{static_cast<device_node_t>(in[0]), static_cast<device_node_t>(in[1]), static_cast<device_node_t>(in[2])};};
 static auto casting_coord2d = [](coord2d in){return device_coord2d{static_cast<device_real_t>(in.first), static_cast<device_real_t>(in.second)};};
@@ -25,20 +26,33 @@ static auto casting_coord2d = [](coord2d in){return device_coord2d{static_cast<d
 
 __global__ void refill_batch_(IsomerBatch B, IsomerBatch Q_B, BatchQueue::QueueProperties queue){
 
+    //Create a shared queue index among all threads in a block. 
     __shared__ size_t queue_index;
+
+    //Store a copy of the front before incrementing the counters, 
+    //This is to allow for unordered incrementing of the queue counters avoiding the use of mutexes or similar techniques. 
     auto old_front = *queue.front;
+
+    //Reset the queue requests at each call to this function.
     if ((threadIdx.x + blockIdx.x) == 0) {
-        *queue.requests = 0; *queue.capacity = Q_B.isomer_capacity;}
+        *queue.requests = 0;}
     GRID_SYNC
+    //Grid stride for loop, allows for handling of any batch size.
     for (int isomer_idx = blockIdx.x; isomer_idx < B.isomer_capacity; isomer_idx+= gridDim.x){
+
+    //Checks if the isomer owned by a given block is finished or empty, if it is we want to replace it with a new one.
     if (B.statuses[isomer_idx] != NOT_CONVERGED){
         if(threadIdx.x == 0){
+            //Increment front counter atomically and calculate queue index. 
             queue_index = atomicAdd(queue.front, 1);
             queue_index = queue_index % *queue.capacity;
+            //Increment number of queue requests.
             atomicAdd(queue.requests, 1);
         } 
         cg::sync(cg::this_thread_block());
         assert(queue_index < *queue.capacity);
+
+        //Given the queue index, copy data from the queue (container Q_B) to the target batch B.
         size_t queue_array_idx    = queue_index*blockDim.x+threadIdx.x;
         size_t global_idx         = blockDim.x*isomer_idx + threadIdx.x;
         reinterpret_cast<device_coord3d*>(B.X)[global_idx]           = reinterpret_cast<device_coord3d*>(Q_B.X)[queue_array_idx];  
@@ -57,11 +71,14 @@ __global__ void refill_batch_(IsomerBatch B, IsomerBatch Q_B, BatchQueue::QueueP
         bool enough_left_in_queue = *queue.size >= *queue.requests;
         *queue.size -= enough_left_in_queue ? *queue.requests : *queue.size;
         if (*queue.size == 0) {*queue.front = -1; *queue.back =-1;} else{
+            //Here we correct the front index by considering, what the front was at the start, how many requests were made and, what the capacity of the queue is.
             *queue.front = enough_left_in_queue ? (old_front + *queue.requests) % *queue.capacity  : *queue.back;
         }
     }
 }
 
+//This function simply copys data to the end of the batch, could be achieved with the cuda_io::copy() function using ranges.
+//TODO: delete this function and replace with a copy call.
 __global__ void insert_batch_(IsomerBatch B, IsomerBatch Q_B, BatchQueue::QueueProperties queue){
     for (size_t isomer_idx = blockIdx.x; isomer_idx < B.isomer_capacity; isomer_idx+=gridDim.x){
     size_t queue_idx = (*queue.back + isomer_idx) % *queue.capacity;
@@ -80,18 +97,20 @@ __global__ void insert_batch_(IsomerBatch B, IsomerBatch Q_B, BatchQueue::QueueP
     }
 }
 
+//Allocate managed (unified) memory for queue counters.
 BatchQueue::BatchQueue(const size_t N): N(N){
     cudaMallocManaged(&props.back,    sizeof(size_t));
     cudaMallocManaged(&props.front,   sizeof(size_t));
     cudaMallocManaged(&props.capacity,sizeof(size_t));
     cudaMallocManaged(&props.size,    sizeof(size_t));
-    cudaMallocManaged(&props.requests,    sizeof(size_t));
+    cudaMallocManaged(&props.requests,sizeof(size_t));
     *props.capacity = 1;
     *props.front = -1;
     *props.back = -1;
     *props.size = 0;
 }
 
+//Free counters
 BatchQueue::~BatchQueue(){
     cudaFree(props.back);
     cudaFree(props.front);
@@ -100,6 +119,7 @@ BatchQueue::~BatchQueue(){
     cudaFree(props.requests);
 }
 
+//Copies data to host if data is not up to date on host
 cudaError_t BatchQueue::to_host(const LaunchCtx& ctx){
     if(!is_host_updated){
         copy(host_batch, device_batch, ctx);
@@ -108,6 +128,7 @@ cudaError_t BatchQueue::to_host(const LaunchCtx& ctx){
     return cudaGetLastError();
 }
 
+//Copies data to device if data is not up to date on device
 cudaError_t BatchQueue::to_device(const LaunchCtx& ctx){
     if(!is_device_updated){
         copy(device_batch, host_batch, ctx);
@@ -116,6 +137,7 @@ cudaError_t BatchQueue::to_device(const LaunchCtx& ctx){
     return cudaGetLastError();
 }
 
+//Kernel wrapper for refill_batch_ function
 cudaError_t BatchQueue::refill_batch(IsomerBatch& batch, const LaunchCtx& ctx, const LaunchPolicy policy){
     if (policy == LaunchPolicy::SYNC) ctx.wait();
     static LaunchDims dims((void*)refill_batch_, N, 0, batch.isomer_capacity);
@@ -128,6 +150,7 @@ cudaError_t BatchQueue::refill_batch(IsomerBatch& batch, const LaunchCtx& ctx, c
     return error;
 }
 
+//Resizes the underlying containers (host and device batches) and updates the queue counters accordingly
 cudaError_t BatchQueue::resize(const size_t new_capacity,const LaunchCtx& ctx, const LaunchPolicy policy){
     //Lambda function because it is only called in here and avoids duplication of code because these transformations need to be applied to both host and device batches.
     auto queue_resize_batch = [&](IsomerBatch& batch){
@@ -159,76 +182,100 @@ cudaError_t BatchQueue::resize(const size_t new_capacity,const LaunchCtx& ctx, c
     return cudaGetLastError();
 }
 
+//Inserts entire IsomerBatch in the queue.
 cudaError_t BatchQueue::insert(IsomerBatch& input_batch, const LaunchCtx& ctx, const LaunchPolicy policy, const bool insert_2d){
+    //Wait for all processes on the stream to finish if policy is synchronous otherwise just proceed.
     if(policy == LaunchPolicy::SYNC) ctx.wait(); 
+
+    //Static storage specifier of LaunchDims object allows us to perform the calculations on first function call,
+    // and store the result for future calls. Thus avoiding having to perform this calculation outside and pass the state to the function.
     static LaunchDims dims((void*)insert_batch_, N, 0, input_batch.isomer_capacity);
+
     input_batch.buffer_type == DEVICE_BUFFER ? to_device(ctx) : to_host(ctx); 
     if (*props.capacity < (*props.size + input_batch.isomer_capacity))
     {
         *props.capacity += input_batch.isomer_capacity;
         resize(*props.capacity, ctx, policy);
     }
-    cudaError_t error;
+
+    //TODO: Maybe we want to implement some inefficient way of copying a host batch to the device.
     if (input_batch.buffer_type == DEVICE_BUFFER){
         void* kargs[]{(void*)&input_batch, (void*)&device_batch, (void*)&props};
-        error = safeCudaKernelCall((void*)insert_batch_,dims.get_grid(), dims.get_block(), kargs, 0, ctx.stream);
+        safeCudaKernelCall((void*)insert_batch_,dims.get_grid(), dims.get_block(), kargs, 0, ctx.stream);
     } else{
         std::cout << "Failed to insert: Batch insertion is intended for device batches only" << std::endl;
     }   
     if(policy == LaunchPolicy::SYNC) ctx.wait();
     printLastCudaError("Batch insertion failed");
     is_host_updated = false;
-    return error;
+    return cudaGetLastError();
 }
 
 cudaError_t BatchQueue::insert(const PlanarGraph& in, const size_t ID, const LaunchCtx& ctx, const LaunchPolicy policy, const bool insert_2d){
+    //Before inserting a new isomer, make sure that the host batch is up to date with the device version.
     to_host(ctx);
-    if (*props.capacity == *props.size)
-    {
+
+    //If the queue is full, double the size, same beahvior as dynamically allocated containers in the std library. 
+    if (*props.capacity == *props.size){
         *props.capacity *= 2;
         resize(*props.capacity, ctx, policy);
     }
+
+    //Edge case: queue has no members 
     if(*props.back == -1){
         *props.front = 0;
         *props.back = 0;
     } else{
     *props.back = *props.back + 1 % *props.capacity;}
+
+    //Extract the graph information (neighbours) from the PlanarGraph object and insert it at the appropriate location in the queue.
     size_t offset = *props.back * N;
     for(int i = 0; i < in.neighbours.size(); i++){
         reinterpret_cast<device_node3*>  (host_batch.neighbours)[offset +i]             = casting_node3(in.neighbours[i]);
         if(insert_2d)   reinterpret_cast<device_coord2d*>(host_batch.xys)[offset + i]   = casting_coord2d(in.layout2d[i]);
     }
+
+    //Assign metadata.
     host_batch.statuses[*props.back] = NOT_CONVERGED;
     host_batch.IDs[*props.back] = ID;
     host_batch.iterations[*props.back] = 0;
     *props.size += 1;
+
+    //Since the host batch has been updated the device is no longer up to date.
     is_device_updated = false;
     return cudaGetLastError();
 }
 
 cudaError_t BatchQueue::insert(const Polyhedron& in, const size_t ID, const LaunchCtx& ctx, const LaunchPolicy policy, const bool insert_2d){
+    //Before inserting a new isomer, make sure that the host batch is up to date with the device version.
     to_host(ctx);
-    if (*props.capacity == *props.size)
-    {
+
+    //If the queue is full, double the size, same beahvior as dynamically allocated containers in the std library. 
+    if (*props.capacity == *props.size){
         *props.capacity *= 2;
         resize(*props.capacity, ctx, policy);
     }
+    //Edge case: queue has no members 
     if(*props.back == -1){
         *props.front = 0;
         *props.back = 0;
     } else{
     *props.back = *props.back + 1 % *props.capacity;}
+
+    //Extract the graph information (neighbours) from the PlanarGraph object and insert it at the appropriate location in the queue.
     size_t offset = *props.back * N;
     for(int i = 0; i < in.neighbours.size(); i++){
         if(!in.neighbours.empty())              reinterpret_cast<device_node3*>  (host_batch.neighbours)[offset +i]  = casting_node3(in.neighbours[i]);
         if(!in.points.empty())                  reinterpret_cast<device_coord3d*>(host_batch.X)[offset + i]          = casting_coord3d(in.points[i]);
         if(!in.layout2d.empty() && insert_2d)   reinterpret_cast<device_coord2d*>(host_batch.xys)[offset + i]        = casting_coord2d(in.layout2d[i]);
     }
+    //Assign metadata.
     host_batch.statuses[*props.back] = NOT_CONVERGED;
     host_batch.IDs[*props.back] = ID;
     host_batch.iterations[*props.back] = 0;
 
     *props.size += 1;
+    //Since the host batch has been updated the device is no longer up to date.
     is_device_updated = false;
     return cudaGetLastError();
 }
