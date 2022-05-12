83c950c9eaf48d818bb5b9835c57be8213309d74
Jonaslacour <jonaslacour1@gmail.com>
2022-05-12 16:09:08 +0200

    Added: New testing functions, is_close(IsomerBatch, IsomerBatch), num_converged(IsomerBatch), num_failed(IsomerBatch), sort(IsomerBatch) and fixed floating point comparison in the case when both are 0, inf or nan

--- include/fullerenes/gpu/cuda_io.hh
+++ include/fullerenes/gpu/cuda_io.hh
@@ -7,4 +7,8 @@ namespace cuda_io{
     cudaError_t copy(IsomerBatch& target, const IsomerBatch& source, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC, const std::pair<int,int>& lhs_range = {-1,-1}, const std::pair<int,int>& rhs_range = {-1,-1});
     cudaError_t resize(IsomerBatch& batch, size_t new_capacity, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC, int front = -1, int back = -1);
     cudaError_t reset_convergence_statuses(IsomerBatch& batch, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
+    void is_close(const IsomerBatch& a, const IsomerBatch& b, device_real_t tol = 0.0, bool verbose = false);
+    int num_converged(const IsomerBatch& batch);
+    int num_failed(const IsomerBatch& batch);
+    void sort(IsomerBatch& batch);
 }
