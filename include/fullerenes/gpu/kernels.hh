d0441f9d132758237a83a4e98fa63cb88b79aba9
Jonaslacour <jonaslacour1@gmail.com>
2022-05-27 17:51:55 +0200

    Added: 10 new GPU based statistical functions calculating means, rms, max of bond-error, angle-error, dihedral-error or magnitude of energy or gradient

--- include/fullerenes/gpu/kernels.hh
+++ include/fullerenes/gpu/kernels.hh
@@ -1,12 +1,30 @@
 #include "isomerspace_kernel.hh"
 #include "cuda_execution.hh"
+#include "cu_array.hh"
+
+enum ForcefieldType {WIRZ, BUSTER, FLATNESS_ENABLED};
+
 namespace gpu_kernels{
     namespace isomerspace_forcefield{
         int optimal_batch_size(const int N, const int device_id = 0);
+
         //Returns total time spent in kernel in milliseconds. First call is not timed as that would require synchronous execution.
         float time_spent(); 
         //Uses forcefield optimization to relax the positions of atoms in all isomers of a batch.
         cudaError_t optimize_batch(IsomerBatch& B, const size_t iterations, const size_t max_iterations, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
+
+        //Stat functions:
+        cudaError_t get_bond_rms        (const IsomerBatch& B, CuArray<device_real_t>& bond_rms);
+        cudaError_t get_angle_rms       (const IsomerBatch& B, CuArray<device_real_t>& angle_rms);
+        cudaError_t get_dihedral_rms    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_rms);
+        cudaError_t get_bond_max        (const IsomerBatch& B, CuArray<device_real_t>& bond_max);
+        cudaError_t get_angle_max       (const IsomerBatch& B, CuArray<device_real_t>& angle_max);    
+        cudaError_t get_dihedral_max    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_max);
+        cudaError_t get_energies        (const IsomerBatch& B, CuArray<device_real_t>& energies);
+        cudaError_t get_gradient_norm   (const IsomerBatch& B, CuArray<device_real_t>& gradient_norm);
+        cudaError_t get_gradient_rms    (const IsomerBatch& B, CuArray<device_real_t>& gradient_rms);
+        cudaError_t get_gradient_max    (const IsomerBatch& B, CuArray<device_real_t>& gradient_max);
+
     }
 
     namespace isomerspace_X0{
