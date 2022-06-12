0e012507b9b6276f0189795f134fe71b251b0cce
Jonaslacour <jonaslacour1@gmail.com>
2022-06-12 15:59:21 +0200

    Changed: improved dualisation performance through cache utilisation instead of dram, also fixed unitialised memory errors and race condition

--- src/cuda/isomerspace_dualize.cu
+++ src/cuda/isomerspace_dualize.cu
@@ -17,18 +17,24 @@ __global__
 void cubic_layout_(IsomerBatch B){
     DEVICE_TYPEDEFS
     extern __shared__  real_t sharedmem[];
-    __shared__ device_node_t* triangle_numbers;
+    device_node_t* triangle_numbers = reinterpret_cast<device_node_t*>(sharedmem);
+    device_node_t* smem = triangle_numbers + B.n_faces*6;
+    //__shared__ device_node_t* triangle_numbers;
+    //
+    //if(threadIdx.x == 0) {
+    //    triangle_numbers = (device_node_t*)malloc(B.n_faces*6*sizeof(device_node_t));
+    //    memset(triangle_numbers, 0, sizeof(device_node_t)*B.n_faces*6);
+    //}
 
-    if(threadIdx.x == 0) triangle_numbers = (device_node_t*)malloc(B.n_faces*6*sizeof(device_node_t));
 
     BLOCK_SYNC
     for (int isomer_idx = blockIdx.x; isomer_idx < B.isomer_capacity; isomer_idx += gridDim.x ){
     if (B.statuses[isomer_idx] != EMPTY ){
-    device_node_t* smem = reinterpret_cast<device_node_t*>(sharedmem);
+    BLOCK_SYNC
     DeviceFullereneDual FD(B.dual_neighbours + isomer_idx * B.n_faces * 6, B.face_degrees + isomer_idx * B.n_faces);
     auto thid = threadIdx.x;
     device_node_t cannon_arcs[6]; memset(cannon_arcs,0,sizeof(device_node_t)*6);
-    bool is_representative[6]; 
+    bool is_representative[6]; memset(is_representative,0,sizeof(device_node_t)*6);
     int represent_count = 0;
     if(thid < B.n_faces){
         for (int i = 0; i < FD.face_degrees[thid]; ++i){
@@ -37,8 +43,6 @@ void cubic_layout_(IsomerBatch B){
                 cannon_arcs[i] = cannon_arc.y;
                 is_representative[i] = true;
                 represent_count++;
-            } else {
-                is_representative[i] = false;
             }
         }
     }
@@ -54,7 +58,7 @@ void cubic_layout_(IsomerBatch B){
         }
     }
     BLOCK_SYNC
-    device_node2* representative_arc_list = reinterpret_cast<device_node2*>(sharedmem);
+    device_node2* representative_arc_list = reinterpret_cast<device_node2*>(smem);
     device_node_t node_id;
     if (thid < B.n_faces){
         int arc_count = 0;
@@ -80,7 +84,6 @@ void cubic_layout_(IsomerBatch B){
         };
         }
     }*/
-    BLOCK_SYNC
     auto [u, v] = representative_arc_list[thid];
     device_node_t w = FD.next(u,v);
 
@@ -104,7 +107,7 @@ cudaError_t cubic_layout(IsomerBatch& B, const LaunchCtx& ctx, const LaunchPolic
     kernel_time += single_kernel_time;
 
     if(policy == LaunchPolicy::SYNC) ctx.wait();
-    size_t smem = sizeof(device_coord2d)*B.n_atoms*2 + sizeof(device_real_t)*Block_Size_Pow_2;
+    size_t smem = sizeof(device_coord3d)*B.n_atoms*3 + sizeof(device_real_t)*Block_Size_Pow_2;
     static LaunchDims dims((void*)cubic_layout_, B.n_atoms, smem, B.isomer_capacity);
     dims.update_dims((void*)cubic_layout_, B.n_atoms, smem, B.isomer_capacity);
     cudaError_t error;
