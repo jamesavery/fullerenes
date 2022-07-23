d7b2e97f1ae2569a439a8be553cbe25b8656406f
Jonaslacour <jonaslacour1@gmail.com>
2022-07-23 23:30:00 +0200

    Changed: Dualize now caches neighbor list for better performance. Also uses INT_MAX store tactic in representative_arcs instead of seperate boolean array. Described in dualize algorithm.

--- src/cuda/isomerspace_dualize.cu
+++ src/cuda/isomerspace_dualize.cu
@@ -12,78 +12,73 @@ namespace isomerspace_dual{
 #include "coord2d.cuh"
 #include "forcefield_structs.cu"
 #include "print_functions.cu"
+#include "chrono"
 
 __global__
 void cubic_layout_(IsomerBatch B){
+    unsigned int power2 = B.n_faces; // compute the next highest power of 2 of 32-bit power2
+    power2--;
+    power2 |= power2 >> 1;
+    power2 |= power2 >> 2;
+    power2 |= power2 >> 4;
+    power2 |= power2 >> 8;
+    power2 |= power2 >> 16;
+    power2++;
+
     DEVICE_TYPEDEFS
     extern __shared__  real_t sharedmem[];
     device_node_t* triangle_numbers = reinterpret_cast<device_node_t*>(sharedmem);
-    device_node_t* smem = triangle_numbers + B.n_faces*6;
-    //__shared__ device_node_t* triangle_numbers;
-    //
-    //if(threadIdx.x == 0) {
-    //    triangle_numbers = (device_node_t*)malloc(B.n_faces*6*sizeof(device_node_t));
-    //    memset(triangle_numbers, 0, sizeof(device_node_t)*B.n_faces*6);
-    //}
-
-
-    BLOCK_SYNC
+    device_node_t* cached_neighbours = triangle_numbers + B.n_faces*6;
+    uint8_t* cached_degrees = reinterpret_cast<uint8_t*>(cached_neighbours+ B.n_faces*6);
+    device_node_t* smem = reinterpret_cast<device_node_t*>(cached_neighbours) + B.n_faces*8;
+    clear_cache(reinterpret_cast<real_t*>(smem),power2);
     for (int isomer_idx = blockIdx.x; isomer_idx < B.isomer_capacity; isomer_idx += gridDim.x ){
     if (B.statuses[isomer_idx] != EMPTY ){
+
     BLOCK_SYNC
-    DeviceFullereneDual FD(B.dual_neighbours + isomer_idx * B.n_faces * 6, B.face_degrees + isomer_idx * B.n_faces);
     auto thid = threadIdx.x;
-    device_node_t cannon_arcs[6]; memset(cannon_arcs,0,sizeof(device_node_t)*6);
-    bool is_representative[6]; memset(is_representative,0,sizeof(device_node_t)*6);
+    if (thid < B.n_faces){
+        reinterpret_cast<node6*>(cached_neighbours)[thid] = reinterpret_cast<node6*>(B.dual_neighbours)[thid + isomer_idx*B.n_faces];
+        cached_degrees[thid] = B.face_degrees[thid + isomer_idx*B.n_faces];
+    }
+    DeviceFullereneDual FD(cached_neighbours, cached_degrees);
+    device_node_t cannon_arcs[6]; memset(cannon_arcs, UINT16_MAX,sizeof(device_node_t)*6);
     int represent_count = 0;
+    BLOCK_SYNC
     if(thid < B.n_faces){
         for (int i = 0; i < FD.face_degrees[thid]; ++i){
             device_node2 cannon_arc = FD.get_cannonical_triangle_arc(thid,FD.dual_neighbours[thid*6 + i]);
             if (cannon_arc.x == thid) {
                 cannon_arcs[i] = cannon_arc.y;
-                is_representative[i] = true;
                 represent_count++;
             }
         }
     }
-    exclusive_scan(smem, represent_count);
 
+    ex_scan<device_node_t>(smem,represent_count,B.n_faces);
+    
     if (thid < B.n_faces){
         int arc_count = 0;
         for (size_t i = 0; i < FD.face_degrees[thid]; ++i){
-            if (is_representative[i]) {
+            if (cannon_arcs[i] != UINT16_MAX) {
                 triangle_numbers[thid*6 + i] = smem[thid] + arc_count;
                 ++arc_count;
             }
         }
     }
+
     BLOCK_SYNC
     device_node2* representative_arc_list = reinterpret_cast<device_node2*>(smem);
-    device_node_t node_id;
     if (thid < B.n_faces){
-        int arc_count = 0;
         for (size_t i = 0; i < FD.face_degrees[thid]; i++){
-            if(is_representative[i]){
-                auto idx = triangle_numbers[thid*6 + i] + arc_count;
+            if(cannon_arcs[i] != UINT16_MAX){
+                auto idx = triangle_numbers[thid*6 + i];
                 representative_arc_list[idx] = {device_node_t(thid), cannon_arcs[i]}; 
             }
         }
         
     }
     BLOCK_SYNC
-    /*
-    if(thid < B.n_faces){
-        for (int i = 0; i < FD.face_degrees[thid]; ++i){
-        device_node2 cannon_arc = FD.get_cannonical_triangle_arc(thid,FD.dual_neighbours[thid*6 + i]);
-        if (cannon_arc.x == thid) {
-            device_node_t v(FD.dual_neighbours[thid*6 + i]);
-            device_node_t w = FD.next(thid,v);
-            device_node2 edge_b = FD.get_cannonical_triangle_arc(v, thid); B.cubic_neighbours[isomer_idx*B.n_atoms*3 + triangle_numbers[thid*6 + i]*3 + 0] = triangle_numbers[edge_b.x * 6 + FD.dedge_ix(edge_b.x, edge_b.y)];
-            device_node2 edge_c = FD.get_cannonical_triangle_arc(w, v); B.cubic_neighbours[isomer_idx*B.n_atoms*3 + triangle_numbers[thid*6 + i]*3 + 1] = triangle_numbers[edge_c.x * 6 + FD.dedge_ix(edge_c.x, edge_c.y)];
-            device_node2 edge_d = FD.get_cannonical_triangle_arc(thid, w); B.cubic_neighbours[isomer_idx*B.n_atoms*3 + triangle_numbers[thid*6 + i]*3 + 2] = triangle_numbers[edge_d.x * 6 + FD.dedge_ix(edge_d.x, edge_d.y)];
-        };
-        }
-    }*/
     auto [u, v] = representative_arc_list[thid];
     device_node_t w = FD.next(u,v);
 
@@ -94,8 +89,8 @@ void cubic_layout_(IsomerBatch B){
 }}}
 
 float kernel_time = 0.0;
-float time_spent(){
-    return kernel_time;
+std::chrono::microseconds time_spent(){
+    return std::chrono::microseconds((int) (kernel_time*1000.f));
 }
 
 cudaError_t cubic_layout(IsomerBatch& B, const LaunchCtx& ctx, const LaunchPolicy policy){
@@ -103,23 +98,27 @@ cudaError_t cubic_layout(IsomerBatch& B, const LaunchCtx& ctx, const LaunchPolic
     static cudaEvent_t start, stop;
     float single_kernel_time = 0.0;
     if(first_call) {cudaEventCreate(&start); cudaEventCreate(&stop);}
-    cudaEventElapsedTime(&single_kernel_time, start, stop);
-    kernel_time += single_kernel_time;
 
-    if(policy == LaunchPolicy::SYNC) ctx.wait();
+    if(policy == LaunchPolicy::SYNC) {ctx.wait();}
+    else if(policy == LaunchPolicy::ASYNC){
+        cudaEventElapsedTime(&single_kernel_time, start, stop);
+        kernel_time += single_kernel_time;
+    }
+
     size_t smem = sizeof(device_coord3d)*B.n_atoms*3 + sizeof(device_real_t)*Block_Size_Pow_2;
     static LaunchDims dims((void*)cubic_layout_, B.n_atoms, smem, B.isomer_capacity);
     dims.update_dims((void*)cubic_layout_, B.n_atoms, smem, B.isomer_capacity);
     cudaError_t error;
-    cudaEventRecord(start, ctx.stream);
-
     void* kargs[]{(void*)&B};
+    cudaEventRecord(start, ctx.stream);
     error = safeCudaKernelCall((void*)cubic_layout_, dims.get_grid(), dims.get_block(), kargs, smem, ctx.stream);  
-    cudaDeviceSynchronize();
-    
     cudaEventRecord(stop, ctx.stream);
     
-    if(policy == LaunchPolicy::SYNC) ctx.wait();
+    if(policy == LaunchPolicy::SYNC) {
+        ctx.wait();
+        cudaEventElapsedTime(&single_kernel_time, start, stop);
+        kernel_time += single_kernel_time;
+    }
     printLastCudaError("Cubic Layout: ");
     first_call = false;
     return error;
