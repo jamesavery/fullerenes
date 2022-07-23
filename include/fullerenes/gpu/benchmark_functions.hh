2c227c9535020d1f034b28e0c01f8779ee80dc1a
Jonaslacour <jonaslacour1@gmail.com>
2022-07-23 18:08:02 +0200

    Added: cuda benchmark namespace

---
+++ include/fullerenes/gpu/benchmark_functions.hh
@@ -0,0 +1,11 @@
+#include "isomerspace_kernel.hh"
+#include "chrono"
+namespace cuda_benchmark {
+
+    std::chrono::microseconds benchmark_scan(const int n_elements, const int n_times, const int scan_version);
+
+    std::chrono::nanoseconds benchmark_reduction(const int n_elements, const int n_times, const int reduction_version);
+
+    size_t n_blocks(const int n_elements);
+
+}
