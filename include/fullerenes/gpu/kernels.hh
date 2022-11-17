fe77e70297f39339ac1abb3fb688273b68a20314
Jonaslacour <jonaslacour1@gmail.com>
2022-11-17 19:46:57 +0100

    Added: more forcefield statistics getter functions

--- include/fullerenes/gpu/kernels.hh
+++ include/fullerenes/gpu/kernels.hh
@@ -3,7 +3,7 @@
 #include "cu_array.hh"
 #include "chrono"
 
-enum ForcefieldType {WIRZ, BUSTER, FLATNESS_ENABLED};
+enum ForcefieldType {WIRZ, BUSTER, FLATNESS_ENABLED, FLAT_BOND};
 
 namespace gpu_kernels{
     namespace isomerspace_forcefield{
@@ -12,30 +12,40 @@ namespace gpu_kernels{
         //Returns total time spent in kernel in milliseconds. First call is not timed as that would require synchronous execution.
         std::chrono::microseconds time_spent(); 
         //Uses forcefield optimization to relax the positions of atoms in all isomers of a batch.
+        template <ForcefieldType T>
         cudaError_t optimize_batch(IsomerBatch& B, const size_t iterations, const size_t max_iterations, const LaunchCtx& ctx = LaunchCtx(), const LaunchPolicy policy = LaunchPolicy::SYNC);
         cudaError_t test_fun(IsomerBatch& B, CuArray<device_real_t>& output);
-        //Stat functions:
+        //ForcefieldType agnostic functions since the forcefield type doesnt change the way bonds are computed
+        //Retrieves all interatomic bond lengths, angles and dihedrals
         cudaError_t get_bonds           (const IsomerBatch& B, CuArray<device_real_t>& bonds);          //N x M x 3
         cudaError_t get_angles          (const IsomerBatch& B, CuArray<device_real_t>& angles);         //N x M x 3
         cudaError_t get_dihedrals       (const IsomerBatch& B, CuArray<device_real_t>& dihedrals);      //N x M x 3
 
-        cudaError_t get_energy          (const IsomerBatch& B, CuArray<device_real_t>& energy);         //M x 1
-        cudaError_t get_bond_mean       (const IsomerBatch& B, CuArray<device_real_t>& bond_mean);      //M x 1
-        cudaError_t get_angle_mean      (const IsomerBatch& B, CuArray<device_real_t>& angle_mean);     //M x 1
-        cudaError_t get_dihedral_mean   (const IsomerBatch& B, CuArray<device_real_t>& dihedral_mean);  //M x 1
 
-        cudaError_t get_bond_rms        (const IsomerBatch& B, CuArray<device_real_t>& bond_rms);       //M x 1
-        cudaError_t get_angle_rms       (const IsomerBatch& B, CuArray<device_real_t>& angle_rms);      //M x 1
-        cudaError_t get_dihedral_rms    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_rms);   //M x 1
-        cudaError_t get_bond_max        (const IsomerBatch& B, CuArray<device_real_t>& bond_max);       //M x 1
-        cudaError_t get_angle_max       (const IsomerBatch& B, CuArray<device_real_t>& angle_max);      //M x 1
-        cudaError_t get_dihedral_max    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_max);   //M x 1
-        cudaError_t get_energies        (const IsomerBatch& B, CuArray<device_real_t>& energies);       //M x 1
+
+        //Stat functions:
+        template <ForcefieldType T> cudaError_t get_dihedral_mean   (const IsomerBatch& B, CuArray<device_real_t>& dihedral_mean);  //M x 1
+        template <ForcefieldType T> cudaError_t get_dihedral_rms    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_rms);   //M x 1
+        template <ForcefieldType T> cudaError_t get_dihedral_max    (const IsomerBatch& B, CuArray<device_real_t>& dihedral_max);   //M x 1
+        template <ForcefieldType T> cudaError_t get_energies        (const IsomerBatch& B, CuArray<device_real_t>& energies);       //M x 1
+
+        template <ForcefieldType T> cudaError_t get_energy          (const IsomerBatch& B, CuArray<device_real_t>& energy);         //M x 1
+        template <ForcefieldType T> cudaError_t get_bond_mean       (const IsomerBatch& B, CuArray<device_real_t>& bond_mean);      //M x 1
+        template <ForcefieldType T> cudaError_t get_bond_rms        (const IsomerBatch& B, CuArray<device_real_t>& bond_rms);       //M x 1
+        template <ForcefieldType T> cudaError_t get_bond_max        (const IsomerBatch& B, CuArray<device_real_t>& bond_max);       //M x 1
+        template <ForcefieldType T> cudaError_t get_angle_mean      (const IsomerBatch& B, CuArray<device_real_t>& angle_mean);     //M x 1
+        template <ForcefieldType T> cudaError_t get_angle_rms       (const IsomerBatch& B, CuArray<device_real_t>& angle_rms);      //M x 1
+        template <ForcefieldType T> cudaError_t get_angle_max       (const IsomerBatch& B, CuArray<device_real_t>& angle_max);      //M x 1
         
-        cudaError_t get_gradient_mean   (const IsomerBatch& B, CuArray<device_real_t>& gradient_mean);  //M x 1
-        cudaError_t get_gradient_norm   (const IsomerBatch& B, CuArray<device_real_t>& gradient_norm);  //M x 1
-        cudaError_t get_gradient_rms    (const IsomerBatch& B, CuArray<device_real_t>& gradient_rms);   //M x 1
-        cudaError_t get_gradient_max    (const IsomerBatch& B, CuArray<device_real_t>& gradient_max);   //M x 1
+        template <ForcefieldType T> cudaError_t get_flat_mean       (const IsomerBatch& B, CuArray<device_real_t>& flat_mean);      //M x 1
+        template <ForcefieldType T> cudaError_t get_flat_max        (const IsomerBatch& B, CuArray<device_real_t>& flat_max);      //M x 1
+        template <ForcefieldType T> cudaError_t get_flat_rms        (const IsomerBatch& B, CuArray<device_real_t>& flat_rms);      //M x 1
+        
+        
+        template <ForcefieldType T> cudaError_t get_gradient_mean   (const IsomerBatch& B, CuArray<device_real_t>& gradient_mean);  //M x 1
+        template <ForcefieldType T> cudaError_t get_gradient_norm   (const IsomerBatch& B, CuArray<device_real_t>& gradient_norm);  //M x 1
+        template <ForcefieldType T> cudaError_t get_gradient_rms    (const IsomerBatch& B, CuArray<device_real_t>& gradient_rms);   //M x 1
+        template <ForcefieldType T> cudaError_t get_gradient_max    (const IsomerBatch& B, CuArray<device_real_t>& gradient_max);   //M x 1
 
     }
 
