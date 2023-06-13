b817140b636b39f11433e0de9929a1f739f0d3c7
James Avery <avery@nbi.ku.dk>
2023-06-13 21:31:01 +0200

    Much improved eigenvalues.

--- src/cuda/sym_mat_3.cu
+++ src/cuda/sym_mat_3.cu
@@ -2,6 +2,8 @@
 #include <algorithm>
 struct symMat3
 {
+  static constexpr device_real_t epsilon = std::numeric_limits<device_real_t>::epsilon();
+  
   device_real_t a, b, c, d, e, f;
   
   //[[a , b,  c]
@@ -10,84 +12,79 @@ struct symMat3
   INLINE symMat3(): a(0), b(0), c(0), d(0), e(0), f(0){}
   INLINE symMat3(device_real_t a, device_real_t b, device_real_t c, device_real_t d, device_real_t e, device_real_t f) : a(a), b(b), c(c), d(d), e(e), f(f){}
   
-  //Approx 107 FLOPS
   INLINE device_coord3d eigenvalues() const{
     DEVICE_TYPEDEFS
-     // Coefficients of characteristic polynomial, calculated with Mathematica
-    real_t 
-      A = -1.f,
+
+      // Combined home-derived eigenvalue w/ Wikipedia method
+    long double 
+      A = -1.L,
       B = a+d+f,
       C = b*b + c*c - a*d + e*e - a*f - d*f,
-      D = -c*c*d + real_t(2.)*b*c*e - a*e*e - b*b*f + a*d*f;
+      D = -c*c*d + 2*b*c*e - a*e*e - b*b*f + a*d*f;
 
-    if(ABS(D) < real_t(1e3*std::numeric_limits<real_t>::epsilon())){
-      auto temp = B*B - real_t(4.)*A*C;
-      real_t Disc = temp > real_t(0.) ? SQRT(B*B - real_t(4.)*A*C) : real_t(0.);
-
-      return {0., (-B-Disc)/( real_t(2.)*A),(-B+Disc)/( real_t(2.)*A)};
+    if(fabs(D) < 100*epsilon){			
+      double Disc = SQRT(B*B-4*A*C); // TODO: Kahan's formula for b^2-4ac.
+      return {{0,real_t(-0.5L*(-B-Disc)), real_t(-0.5L*(-B+Disc))}};
     }
+      
+    device_real_t p1 = b*b + c*c + e*e;
+    if(fabs(p1) < 100*epsilon){
+      return {{a,d,f}};
+     }
 
-    // Depress characteristic polynomial - see http://en.wikipedia.org/wiki/Cubic_equation#Reduction_to_a_depressed_cubic
-    real_t
-      p  = ( real_t(3.)*A*C - B*B)/( real_t(3.)*A*A),
-      q  = ( real_t(2.)*B*B*B - real_t(9.)*A*B*C + real_t(27.)*A*A*D)/( real_t(27.)*A*A*A),
-      xc = B/( real_t(3.)*A);
+    device_real_t q = (a+d+f)/3.L; // q=Tr(M)/3
+    device_real_t p2 = (a-q)*(a-q) + (d-q)*(d-q) + (f-q)*(f-q) + 2.L*p1;
+    device_real_t p = SQRT(p2/6.L);
 
-    // François Viète's solution to cubic polynomials with three real roots. 
-    device_coord3d t;
-    if(ABS(p) < real_t(1e3*std::numeric_limits<real_t>::epsilon())) {
-      t = {real_t(0.), real_t(0.), real_t(0.)};
-      return t - xc;}
+    device_real_t detBxp3 = -c*c*(d-q) + 2*b*c*e - (a-q)*e*e - b*b*(f-q) + (a-q)*(d-q)*(f-q);
+    device_real_t r = detBxp3/(2*p*p*p);
 
-    //For numerical stability we must ensure that acos doesn't receive an arugment which is outside [-1,1]
-    auto frac = ( real_t(3.)*q)/( real_t(2.)*p)*SQRT(real_t(-3.)/p);
-    frac = d_max(real_t(-1.),d_min(real_t(1.), frac));
+    device_real_t phi = r <= -1? M_PI/3.L : (r >= 1? 0.L : ACOS(r)/3.L);
+    device_real_t lam1 = q + 2*p*COS(phi);
+    device_real_t lam3 = q + 2*p*COS(phi +  (2.L/3.L)*M_PI);
+    device_real_t lam2 = 3*q - lam1 - lam3;
 
-    real_t K = real_t(2.)*SQRT(-p/ real_t(3.)), 
-                  theta0 = (real_t(1.)/ real_t(3.))*real_t(ACOS(frac));
-    for(int k=0;k<3;k++) d_set(t,k,K*COS(theta0- k* real_t(2.)* real_t(M_PI)/ real_t(3.)) );
-    // lambda = t - B/(3A)
-    return t - xc;
-    
+    return {{lam1,lam2,lam3}};
   }
 
   INLINE std::array<double,3> eigenvalues_fp64() const {
     DEVICE_TYPEDEFS
-     // Coefficients of characteristic polynomial, calculated with Mathematica
-    double 
-      A = -1.f,
+
+      // Combined home-derived eigenvalue w/ Wikipedia method
+    long double 
+      A = -1.L,
       B = a+d+f,
       C = b*b + c*c - a*d + e*e - a*f - d*f,
-      D = -c*c*d + double(2.)*b*c*e - a*e*e - b*b*f + a*d*f;
+      D = -c*c*d + 2*b*c*e - a*e*e - b*b*f + a*d*f;
 
-    if(ABS(D) < double(1e3*std::numeric_limits<double>::epsilon())){
-      double temp = B*B - double(4.)*A*C;
-      double Disc = temp > double(0.) ? SQRT(B*B - double(4.)*A*C) : double(0.);
-
-      return {0., (-B-Disc)/( double(2.)*A),(-B+Disc)/( double(2.)*A)};
+    if(fabs(D) < 100*epsilon){			
+      double Disc = SQRT(B*B-4*A*C); // TODO: Kahan's formula for b^2-4ac.
+      return {{0,double(-0.5L*(-B-Disc)), double(-0.5L*(-B+Disc))}};
     }
+      
+    double p1 = b*b + c*c + e*e;
+    if(fabs(p1) < 100*epsilon){
+      return {{a,d,f}};
+     }
+      
+    double p1 = b*b + c*c + e*e;
+    if(fabs(p1) < 100*epsilon){
+      return {{a,d,f}};
+     }
 
-    // Depress characteristic polynomial - see http://en.wikipedia.org/wiki/Cubic_equation#Reduction_to_a_depressed_cubic
-    double
-      p  = ( double(3.)*A*C - B*B)/( double(3.)*A*A),
-      q  = ( double(2.)*B*B*B - double(9.)*A*B*C + double(27.)*A*A*D)/( double(27.)*A*A*A),
-      xc = B/( double(3.)*A);
+    double q = (a+d+f)/3L; // q=Tr(M)/3
+    double p2 = (a-q)*(a-q) + (d-q)*(d-q) + (f-q)*(f-q) + 2*p1;
+    double p = SQRT(p2/6);
 
-    // François Viète's solution to cubic polynomials with three real roots. 
-    std::array<double,3> t;
-    if(ABS(p) < double(1e3*std::numeric_limits<double>::epsilon())) {
-      t = {double(0.), double(0.), double(0.)};
-      return {t[0]- xc, t[1] - xc, t[2] - xc};}
+    double detBxp3 = -c*c*(d-q) + 2*b*c*e - (a-q)*e*e - b*b*(f-q) + (a-q)*(d-q)*(f-q);
+    double r = detBxp3/(2*p*p*p);
 
-    //For numerical stability we must ensure that acos doesn't receive an arugment which is outside [-1,1]
-    double frac = ( double(3.)*q)/( double(2.)*p)*sqrt(double(-3.)/p);
-    frac = std::clamp(frac, double(-1), double(1.0));
+    double phi = r <= -1? M_PI/3L : (r >= 1? 0L : ACOS(r)/3L);
+    double lam1 = q + 2*p*COS(phi);
+    double lam3 = q + 2*p*COS(phi +  (2L/3L)*M_PI);
+    double lam2 = 3*q - lam1 - lam3;
 
-    double K = double(2.)*sqrt(-p/ double(3.)), 
-                  theta0 = (double(1.)/ double(3.))*double(acos(frac));
-    for(int k=0;k<3;k++) t[k] = K*cos(theta0- k* double(2.)* double(M_PI)/ double(3.));
-    // lambda = t - B/(3A)
-    return {t[0]- xc, t[1] - xc, t[2] - xc};
+    return {{lam1,lam2,lam3}};      
   }
   //Best case 25 FLOPS
   INLINE device_coord3d eigenvector(const device_real_t lambda) const{
@@ -96,13 +93,13 @@ struct symMat3
     // [ a_12 * a_23 - a_13 * (a_22 - r) ]
     // [ a_12 * a_13 - a_23 * (a_11 - r) ]
     // [ (a_11 - r) * (a_22 - r) - a_12^2 ]
-    real_t mat_norm = SQRT(a*a + b*b + c*c + d*d + e*e + f*f);
-    real_t normx;
+    device_real_t mat_norm = SQRT(a*a + b*b + c*c + d*d + e*e + f*f);
+    device_real_t normx;
     coord3d x = {b*e - c*(d-lambda),
                  b*c - e*(a-lambda),
                  (a-lambda)*(d-lambda) - b*b };
     normx = norm(x);
-    if (normx / mat_norm > (real_t)1e4*std::numeric_limits<real_t>::epsilon()){ // not zero-ish
+    if (normx / mat_norm > (device_real_t)1e4*std::numeric_limits<device_real_t>::epsilon()){ // not zero-ish
       return x/normx;
     }
   
@@ -114,7 +111,7 @@ struct symMat3
                  c*c - (a-lambda)*(f-lambda),
                  e*(a-lambda) - b*c };
     normx = norm(x);
-    if (normx / mat_norm > (real_t)1e4*std::numeric_limits<real_t>::epsilon()){ // not zero-ish
+    if (normx / mat_norm > (device_real_t)1e4*std::numeric_limits<device_real_t>::epsilon()){ // not zero-ish
       return x/normx;
     }
 
@@ -126,7 +123,7 @@ struct symMat3
                  b*(f-lambda) - c*e,
                  c*(d-lambda) - b*e };
     normx = norm(x);
-    if (normx / mat_norm > (real_t)1e4*std::numeric_limits<real_t>::epsilon()){ // not zero-ish
+    if (normx / mat_norm > (device_real_t)1e4*std::numeric_limits<device_real_t>::epsilon()){ // not zero-ish
       return x/normx;
     } 
     //assert(false); // Something went wrong possibly two degenerate evals.
@@ -145,7 +142,7 @@ struct symMat3
     // [ a_12 * a_23 - a_13 * (a_22 - r) ]
     // [ a_12 * a_13 - a_23 * (a_11 - r) ]
     // [ (a_11 - r) * (a_22 - r) - a_12^2 ]
-    real_t normx;
+    device_real_t normx;
     coord3d x1 = {b*e - c*(d-lambda),
                  b*c - e*(a-lambda),
                  (a-lambda)*(d-lambda) - b*b };
@@ -158,9 +155,9 @@ struct symMat3
                  b*(f-lambda) - c*e,
                  c*(d-lambda) - b*e };
 
-    real_t err1 = norm(mul(x1)/x1 - lambda);
-    real_t err2 = norm(mul(x2)/x2 - lambda);
-    real_t err3 = norm(mul(x3)/x3 - lambda);
+    device_real_t err1 = norm(mul(x1)/x1 - lambda);
+    device_real_t err2 = norm(mul(x2)/x2 - lambda);
+    device_real_t err3 = norm(mul(x3)/x3 - lambda);
     if( threadIdx.x + blockIdx.x == 0){
       coord3d lambdax1 = mul(x1)/x1; 
       coord3d lambdax2 = mul(x2)/x2;
