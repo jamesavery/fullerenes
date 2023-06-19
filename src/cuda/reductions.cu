96011f454e7ea378dd314f5ea04403b9a7602382
Jonas la Cour <jonaslacour1@gmail.com>
2023-06-20 00:29:49 +0200

    Reduction max now uses same procedure as the standard reduction (addition).

--- src/cuda/reductions.cu
+++ src/cuda/reductions.cu
@@ -103,23 +103,25 @@ namespace cg = cooperative_groups;
 //The function takes as input a pointer to a shared memory array (sdata) and threadIdx.x's data (data).
 //The function returns the maximum value of the data set.
 __device__ device_real_t reduction_max(device_real_t* sdata, const device_real_t data){
-    sdata[threadIdx.x] = data;
-    cg::thread_block block = cg::this_thread_block();
-    cg::sync(block);
-    
-    if((Block_Size_Pow_2 > 512)){if (threadIdx.x < 512){sdata[threadIdx.x] = d_max(sdata[threadIdx.x + 512],sdata[threadIdx.x]);} cg::sync(block);}
-    if((Block_Size_Pow_2 > 256)){if (threadIdx.x < 256){sdata[threadIdx.x] = d_max(sdata[threadIdx.x + 256],sdata[threadIdx.x]);} cg::sync(block);}
-    if((Block_Size_Pow_2 > 128)){if (threadIdx.x < 128){sdata[threadIdx.x] = d_max(sdata[threadIdx.x + 128],sdata[threadIdx.x]);} cg::sync(block);}
-    if((Block_Size_Pow_2 > 64)){if (threadIdx.x < 64){sdata[threadIdx.x] = d_max(sdata[threadIdx.x + 64],sdata[threadIdx.x]);} cg::sync(block);}
-    if(threadIdx.x < 32){
-    if((Block_Size_Pow_2 > 32)){if (threadIdx.x < 32){sdata[threadIdx.x] = d_max(sdata[threadIdx.x + 32],sdata[threadIdx.x]);} __syncwarp();}
-    cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(block);
-    sdata[threadIdx.x] = cg::reduce(tile32, sdata[threadIdx.x], cg::greater<device_real_t>()); 
-    }
-    cg::sync(block);
-    device_real_t max = sdata[0];
-    cg::sync(block);
-    return max;
+     auto num_warps = (blockDim.x >> 5) + 1;
+        cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cg::this_thread_block());
+        auto warpid = threadIdx.x >> 5;
+        auto laneid = threadIdx.x & 31;
+        device_real_t temp = cg::reduce(tile32, data, cg::greater<device_real_t>());
+        if (num_warps > 1){
+        //sdata[warpid + blockDim.x] = temp;
+        BLOCK_SYNC
+        if (laneid == 0) sdata[warpid] = temp;
+        BLOCK_SYNC
+        if (warpid == 0) {
+            temp = cg::reduce(tile32, sdata[laneid], cg::greater<device_real_t>());
+        }
+        }
+        if (threadIdx.x == 0) sdata[0] = temp;
+        BLOCK_SYNC
+        auto result = sdata[0];
+        BLOCK_SYNC
+        return result;
 }
 
 __device__ device_node_t reduction_max(device_node_t* sdata, const device_node_t data){
