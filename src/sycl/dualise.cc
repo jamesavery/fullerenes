b85a2086bb0ca133d92e374471aa0387a6cce3aa
Jonas la Cour <jonaslacour1@gmail.com>
2023-09-19 14:18:27 +0200

    Fully working sycl dualise

--- src/sycl/dualise.cc
+++ src/sycl/dualise.cc
@@ -1,5 +1,14 @@
 #include <CL/sycl.hpp>
+#include "numeric"
+#include <vector>
+#include <tuple>
+#include <iterator>
+#include <type_traits>
+#include <fullerenes/sycl-isomer-batch.hh>
 #include "forcefield-includes.cc"
+
+//Template specialisation for dualise
+
 
 #define UINT_TYPE_MAX std::numeric_limits<UINT_TYPE>::max()
 
@@ -9,9 +18,9 @@ struct DeviceDualGraph{
     INT_TYPEDEFS(K);
 
     const K* dual_neighbours;                          //(Nf x MaxDegree)
-    const uint8_t* face_degrees;                            //(Nf x 1)
+    const K* face_degrees;                            //(Nf x 1)
     
-    DeviceDualGraph(const K* dual_neighbours, const uint8_t* face_degrees) : dual_neighbours(dual_neighbours), face_degrees(face_degrees) {}
+    DeviceDualGraph(const K* dual_neighbours, const K* face_degrees) : dual_neighbours(dual_neighbours), face_degrees(face_degrees) {}
 
     K dedge_ix(const K u, const K v) const{
         for (uint8_t j = 0; j < face_degrees[u]; j++){
@@ -80,7 +89,6 @@ struct DeviceDualGraph{
         return min_edge;
     }
 };
-
 template <typename T, typename K>
 void dualise(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy policy){
     constexpr int MaxDegree = 6;
@@ -89,6 +97,8 @@ void dualise(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy policy){
     Q.submit([&](handler &h) {
         auto N = batch.N();
         auto Nf = batch.Nf();
+        auto capacity = batch.capacity();
+        std::cout << "Entered dualise" << std::endl;
         
         //Create local accessors to shared memory
         local_accessor<node_t, 1>   triangle_numbers(Nf*MaxDegree, h);
@@ -97,20 +107,22 @@ void dualise(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy policy){
         local_accessor<node2, 1>    arc_list(N, h);
 
         //Create device accessors
-        accessor<node_t, 1>     cubic_neighbours_dev(batch.cubic_neighbours, h, write_only);
-        accessor<node_t, 1>     face_degrees_dev(batch.face_degrees, h, read_only);
-        accessor<node_t, 1>     dual_neighbours_dev(batch.dual_neighbours, h, read_only);
-
-        h.parallel_for<class dualise>(sycl::nd_range(sycl::range{N*batch.capacity()}, sycl::range{N}), [=](nd_item<1> nditem) {
+        accessor     cubic_neighbours_dev(batch.cubic_neighbours, h, write_only);
+        accessor     face_degrees_dev(batch.face_degrees, h, read_only);
+        accessor     dual_neighbours_dev(batch.dual_neighbours, h, read_only);
+        /* 
+        std::cout << N * capacity << std::endl; */
+        h.parallel_for<class dualise>(sycl::nd_range(sycl::range{N*capacity}, sycl::range{N}), [=](nd_item<1> nditem) {
             auto cta = nditem.get_group();
             auto thid = nditem.get_local_linear_id();
             auto bid = nditem.get_group_linear_id();
-            cta.async_work_group_copy(cached_neighbours.get_pointer(), cubic_neighbours_dev.get_pointer() + bid*Nf*MaxDegree, Nf*MaxDegree);
+            cta.async_work_group_copy(cached_neighbours.get_pointer(), dual_neighbours_dev.get_pointer() + bid*Nf*MaxDegree, Nf*MaxDegree);
             cta.async_work_group_copy(cached_degrees.get_pointer(), face_degrees_dev.get_pointer() + bid*Nf, Nf);
             DeviceDualGraph<MaxDegree, node_t> FD(cached_neighbours.get_pointer(), cached_degrees.get_pointer());
             node_t cannon_arcs[MaxDegree]; memset(cannon_arcs, std::numeric_limits<node_t>::max(), MaxDegree*sizeof(node_t));
             node_t rep_count  = 0;
             sycl::group_barrier(cta);
+
             if (thid < Nf){
                 for (node_t i = 0; i < FD.face_degrees[thid]; i++){
                     auto cannon_arc = FD.get_cannonical_triangle_arc(thid, FD.dual_neighbours[thid*MaxDegree + i]);
@@ -151,8 +163,9 @@ void dualise(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy policy){
             auto edge_b = FD.get_cannonical_triangle_arc(v, u); cubic_neighbours_dev[bid*N*3 + thid*3 + 0] = triangle_numbers[edge_b[0]*MaxDegree + FD.dedge_ix(edge_b[0], edge_b[1])];
             auto edge_c = FD.get_cannonical_triangle_arc(w, v); cubic_neighbours_dev[bid*N*3 + thid*3 + 1] = triangle_numbers[edge_c[0]*MaxDegree + FD.dedge_ix(edge_c[0], edge_c[1])];
             auto edge_d = FD.get_cannonical_triangle_arc(u, w); cubic_neighbours_dev[bid*N*3 + thid*3 + 2] = triangle_numbers[edge_d[0]*MaxDegree + FD.dedge_ix(edge_d[0], edge_d[1])];
-
         });
     });
     if(policy == LaunchPolicy::SYNC) Q.wait();
 }
+
+template void dualise<float,uint16_t>(sycl::queue&Q, IsomerBatch<float,uint16_t>& batch, const LaunchPolicy policy);
