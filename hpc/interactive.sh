92318f84f54ca7da07e45a7e67959085de01c6c7
James Avery <avery@diku.dk>
2023-11-22 18:50:49 +0200

    Script for interactive access to compute node, allocate 1 GPU and 14 CPU cores

--- hpc/interactive.sh
+++ hpc/interactive.sh
@@ -1,1 +1,1 @@
-srun --account=project_465000815 --partition=small-g --gpus-per-node=1 --time=00:45:00 --nodes=1 --pty bash
+srun --account=project_465000815 --partition=dev-g --gpus-per-node=1 --cpus-per-task=14 --time=00:45:00 --nodes=1 --pty bash
