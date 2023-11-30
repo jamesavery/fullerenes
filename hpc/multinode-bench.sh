1dcd93930ec87f8e047e7dfd9397b54eddcf1783
James Avery <avery@diku.dk>
2023-11-30 14:48:13 +0200

    No way, det her virker!

--- hpc/multinode-bench.sh
+++ hpc/multinode-bench.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=benchmark.o%j  # Name of stdout output file
 #SBATCH --error=benchmark.e%j   # Name of stderr error file
 #SBATCH --partition=standard-g  # Partition (queue) name
-#SBATCH --nodes=2               # Total number of nodes 
-#SBATCH --ntasks-per-node=8     # 8 MPI ranks per node, 16 total (2x8)
-#SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
+#SBATCH --nodes=@NNODES@               # Total number of nodes 
+#SBATCH --ntasks-per-node=@NTASKSPERNODE@    # 8 MPI ranks per node, 16 total (2x8)
+#SBATCH --gpus-per-node=@NTASKSPERNODE@       # Allocate one gpu per MPI rank
 #SBATCH --cpus-per-task=4 
 #SBATCH --time=0-00:30:00       # Run time (d-hh:mm:ss)
 #SBATCH --account=project_465000815  # Project for billing
@@ -25,6 +25,11 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
+export N_TASKS=@N_TASKS@ # Total number of tasks
+export MY_TASK_ID=$SLURM_PROCID
 
-srun --cpu-bind=${CPU_BIND} ${SCRIPTDIR}/select_gpu.sh ${SCRIPTDIR}/run-bench.sh ${RUNDIR}/test/sycl-tests/sycl-pipeline-multinode $SLURM_LOCALID
+shift 
+srun --cpu-bind=${CPU_BIND} ${SCRIPTDIR}/select_gpu.sh ${SCRIPTDIR}/run-bench.sh $@
+
+#  sbatch ~/fullerenes/hpc/multinode-bench.sh ./benchmarks/sycl/sycl-benchmark 128 
 
---
+++ hpc/run-bench.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+buildenv
+cd $RUNDIR
+
+$@
