ae5165d3bc2e078d1ccb192ea838660cbed5f482
James Avery <avery@nbi.ku.dk>
2023-12-07 21:26:25 +0100

    Balanced Buckygen benchmarking

--- programs/generate-balanced.cc
+++ programs/generate-balanced.cc
@@ -9,8 +9,9 @@ using namespace std::chrono;
 vector<size_t> m_samples(size_t M, size_t m, int seed=42)
 {
   vector<size_t> full_range(M), sample_indices(m);
-  for(size_t i=0;i<M;i++) full_range[i] = i;  
-  auto rng = std::default_random_engine(seed); 
+  for(size_t i=0;i<M;i++) full_range[i] = i;
+  std::mt19937 rng(seed);
+
   shuffle(full_range.begin(),full_range.end(),rng);
   for(size_t i=0;i<m;i++) sample_indices[i] = full_range[i];
   sort(sample_indices.begin(), sample_indices.end());
@@ -20,9 +21,10 @@ vector<size_t> m_samples(size_t M, size_t m, int seed=42)
 
 int main(int ac, char **av)
 {
+  size_t default_M = 1000000, default_m = 10000;
   if(ac<2) {
-    fprintf(stderr,"Syntax: %s <N> [M:1000000] [m:1000] [IPR:0] [only_symmetric:0]\n",
-	    av[0]);
+    fprintf(stderr,"Syntax: %s <N> [M:%ld] [m:%ld] [IPR:0] [only_symmetric:0]\n",
+	    av[0], default_M, default_m);
     return -1;
   }
   bool
@@ -32,27 +34,30 @@ int main(int ac, char **av)
   int     N = strtol(av[1],0,0);  
   int64_t Nisomers = IsomerDB::number_isomers(N,only_symmetric?"Nontrivial":"Any",IPR);
   size_t
-    M = min<size_t>(ac>2? strtol(av[2],0,0):1000000,Nisomers),
-    m = min<size_t>(ac>3? strtol(av[3],0,0):1000,Nisomers);
-    
+    M = min<size_t>(ac>2? strtol(av[2],0,0):default_M,Nisomers),
+    m = min<size_t>(ac>3? strtol(av[3],0,0):default_m,Nisomers);
+  
+  printf("Processing %ld samples out of %ld from the C%d isomerspace (IPR=%d, only_symmetric=%d, Nisomers=%ld).\n",
+	 m,M,N,IPR,only_symmetric,Nisomers);
+  
 
-// State for process_graph
+  // State for buckygen-timing:
  vector<size_t> sample_ix = m_samples(M,m);
  size_t ii=0;
  steady_clock::time_point before_time, after_time;
- duration<double,std::milli> buckygen_time{0.0};
-  
+ using msec = duration<double,milliseconds::period>;
+ msec buckygen_time{0};
+
+ // Processer de grafer, som tilhoerer vores sample-liste
  auto process_graph = [&](const Triangulation &g, size_t cnt){
    // Her kan man smide en masse andet processering ind
- };
- 
-  printf("Grabbing %ld samples out of %ld from the C%d isomerspace (IPR=%d, only_symmetric=%d, Nisomers=%ld).\n",
-	 m,M,N,IPR,only_symmetric,Nisomers);
+ }; 
   
   BuckyGen::buckygen_queue BQ = BuckyGen::start(N,IPR,only_symmetric);
   Triangulation g;
   
   size_t cnt = 0;
+  after_time = steady_clock::now();
   while(BuckyGen::next_fullerene(BQ,g) && cnt < M){
     before_time = steady_clock::now();
 
@@ -62,10 +67,13 @@ int main(int ac, char **av)
       ii++;
     }
     cnt++;
-    
     after_time = steady_clock::now();
   }
-  printf("Generated %ld C%d graphs and processed %ld in %gms (%gus/graph, %gus/vertex)\n",cnt,N,m,buckygen_time.count(),1000*buckygen_time.count()/m,1000*buckygen_time.count()/(m*N));
+  
+  printf("Processed %ld out of %ld C%d-graphs in %gms (%gus/graph, %gus/vertex)\n",
+	 m,cnt,N,
+	 buckygen_time.count(),1000*buckygen_time.count()/m,1000*buckygen_time.count()/(m*N));
+
   
   return 0;
 }
