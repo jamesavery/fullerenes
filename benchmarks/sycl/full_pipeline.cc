9a29aeb4d4e5508e4386d561c5b3f5c4a001a248
Jonas la Cour <jonaslacour1@gmail.com>
2023-12-15 16:15:14 +0100

    We do not know ahead of time, how many isomers the buckyherd queue will generate.

--- benchmarks/sycl/full_pipeline.cc
+++ benchmarks/sycl/full_pipeline.cc
@@ -46,7 +46,8 @@ int main(int argc, char** argv) {
 
     size_t N  = argc>1 ? strtol(argv[1],0,0) : 200;
     size_t NumNodes = argc>2 ? strtol(argv[2],0,0) : 20000000;
-    auto BatchSize = std::ceil((real_t)NumNodes/(real_t)N);
+    size_t NisomersInIsomerspace = IsomerDB::number_isomers(N);
+    auto BatchSize = NisomersInIsomerspace/std::stoi(getenv("N_TASKS"));
 
     size_t Nf = N/2 + 2;
     std::string filename = "output/full_pipeline_" + std::string(getenv("SLURM_JOB_ID")) + "_" + std::string(getenv("MY_TASK_ID")) + "_" + std::string(getenv("N_TASKS")) + ".csv";
@@ -54,43 +55,68 @@ int main(int argc, char** argv) {
 
     sycl::queue Q = sycl::queue(sycl::gpu_selector_v, sycl::property::queue::in_order{});
 
-    size_t n_chunks          = 3; /* Number of chunks per compute node / program instance */ //////3
+    size_t n_chunks          = 6; /* Number of chunks per compute node / program instance */ //////3
     size_t N_chunks          = std::stoi(getenv("N_TASKS"))*n_chunks;                   /* Total number of work chunks */ //
 
     auto my_chunks = loadbalanced_chunks(N_chunks,n_chunks,std::stoi(getenv("MY_TASK_ID")));
     BuckyGen::buckyherd_queue BuckyQ(N,N_chunks,3,
 				   false,false,my_chunks);
-    
-    IsomerBatch<real_t,node_t> batch(N, BatchSize);
-    Graph G(N);
-    auto fill = [&](IsomerBatch<real_t,node_t>& batch)
-    {
-    sycl::host_accessor acc_dual(batch.dual_neighbours, sycl::write_only);
-    sycl::host_accessor acc_degs(batch.face_degrees, sycl::write_only);
-    sycl::host_accessor acc_status (batch.statuses, sycl::write_only);
-    for (size_t ii = 0; ii < BatchSize; ii++)
-    {
-        auto more = BuckyQ.next_fullerene(G);
-        if(!more) break;
 
+    size_t isomers_in_queue = 0;
+    std::vector<node_t> dual_neighbours(BatchSize*1.5*Nf*6);
+    std::vector<node_t> face_degrees(BatchSize*1.5*Nf); //We do not know a priori how many isomers our buckygen queue will generate so we reserve space for 1.5 times the number of isomers we want to generate.
+    
+    
+    Graph G(N);
+    bool more = true;
+    while (more)
+    {
+        more &= BuckyQ.next_fullerene(G);
+        if(!more) break;
         for (size_t j = 0; j < Nf; j++)
         {
             for(size_t k = 0; k < G.neighbours[j].size(); k++)
             {
-                acc_dual[ii*Nf*6 + j*6 + k] = G.neighbours[j][k];
+                //acc_dual[ii*Nf*6 + j*6 + k] = G.neighbours[j][k];
+                dual_neighbours[ii*Nf*6 + j*6 + k] = G.neighbours[j][k];
             } 
             if(G.neighbours[j].size() == 5){
-                acc_dual[ii*Nf*6 + j*6 + 5] = std::numeric_limits<node_t>::max();
-                acc_degs[ii*Nf + j] = 5;
+                //acc_dual[ii*Nf*6 + j*6 + 5] = std::numeric_limits<node_t>::max();
+                dual_neighbours[ii*Nf*6 + j*6 + 5] = std::numeric_limits<node_t>::max();
+                //acc_degs[ii*Nf + j] = 5;
+                face_degrees[ii*Nf + j] = 5;
             } else {
-                acc_degs[ii*Nf + j] = 6;
+                //acc_degs[ii*Nf + j] = 6;
+                face_degrees[ii*Nf + j] = 6;
             }   
 
         }
-        acc_status[ii] = IsomerStatus::NOT_CONVERGED;
+        //acc_status[ii] = IsomerStatus::NOT_CONVERGED;
+        isomers_in_queue++;
     }
 
+    IsomerBatch<real_t,node_t> batch(N, isomers_in_queue);
+
+    auto fill = [&](IsomerBatch<real_t,node_t>& batch)
+    {
+        sycl::host_accessor acc_dual(batch.dual_neighbours, sycl::write_only);
+        sycl::host_accessor acc_degs(batch.face_degrees, sycl::write_only);
+        sycl::host_accessor acc_status (batch.statuses, sycl::write_only);
+        bool more = true;
+        for(size_t ii = 0; ii < isomers_in_queue; ii++)
+            for (size_t j = 0; j < Nf; j++)
+            {
+                for(size_t k = 0; k < 6; k++)
+                {
+                    acc_dual[ii*Nf*6 + j*6 + k] = dual_neighbours[ii*Nf*6 + j*6 + k];
+                    acc_degs[ii*Nf + j] = face_degrees[ii*Nf + j];
+                } 
+            }
+            acc_status[ii] = IsomerStatus::NOT_CONVERGED;
+            isomers_in_queue++;
+        }
     };
+
     
     double times_generate; //Times in nanoseconds.
     double times_memcpy; //Times in nanoseconds.
@@ -115,6 +141,6 @@ int main(int argc, char** argv) {
     auto T5 = std::chrono::steady_clock::now(); times_opt = std::chrono::duration<double, std::nano>(T5 - T4).count();
 
     myfile << "N, Nf, BatchSize, JOBID, NTASKS, TASK_ID, FILL_ME_UP_SCOTTY, MEMCPY, DUAL, TUTTE, PROJECT, OPT\n" << 
-    N << ", " << Nf << ", " << BatchSize << ", " << getenv("SLURM_JOB_ID") << ", " << getenv("N_TASKS") << ", " << getenv("MY_TASK_ID") << ", " << times_generate/BatchSize << ", " << times_memcpy/BatchSize << ", " << times_dual/BatchSize << ", " << times_tutte/BatchSize << ", " << times_project/BatchSize << ", " << times_opt/BatchSize << "\n";
+    N << ", " << Nf << ", " << isomers_in_queue << ", " << getenv("SLURM_JOB_ID") << ", " << getenv("N_TASKS") << ", " << getenv("MY_TASK_ID") << ", " << times_generate/isomers_in_queue << ", " << times_memcpy/isomers_in_queue << ", " << times_dual/isomers_in_queue << ", " << times_tutte/isomers_in_queue << ", " << times_project/isomers_in_queue << ", " << times_opt/isomers_in_queue << "\n";
     return 0;
 }
