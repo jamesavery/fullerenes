e05f16fb5bc7df82b8ad7bf1eab7299c43aeeebb
James Avery <avery@diku.dk>
2024-01-11 10:21:16 +0100

    spiral-to-polyhedron fix

--- programs/spiral-to-polyhedron.cc
+++ programs/spiral-to-polyhedron.cc
@@ -8,15 +8,20 @@ using namespace std;
 
 int main(int ac, char **av)
 {
+  if(ac<=2){
+    fprintf(stderr,"Syntax: %s <spiral> <filename.mol2>\n",av[0]);
+    return -1;
+  }
   string spiral_name = av[1];
   spiral_nomenclature fsn(spiral_name);
-  PlanarGraph g(fsn);
-
+  FullereneGraph g(fsn);
   g.layout2d = g.tutte_layout();
-  Polyhedron P(g,g.zero_order_geometry());
+  auto X = g.zero_order_geometry();
+  Polyhedron P(g,X);
   P.optimize();
   Polyhedron::to_file(P,av[2]);  
 
+  cout <<  P.to_povray() << "\n";
 
     
   return 0;
