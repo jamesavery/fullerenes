11f88c110954665bc6739b8bd518de8b582bdf4b
jonasdelacour <jonaslacour1@gmail.com>
2024-05-02 15:46:00 +0200

    Add timing of new components to full pipeline sycl benchmark

--- benchmarks/sycl/full_pipeline.cc
+++ benchmarks/sycl/full_pipeline.cc
@@ -48,41 +48,51 @@ int main(int argc, char** argv) {
     size_t N  = argc>1 ? strtol(argv[1],0,0) : 200;
     size_t NumNodes = argc>2 ? strtol(argv[2],0,0) : 20000000;
     size_t NisomersInIsomerspace = IsomerDB::number_isomers(N);
-    auto IsomerPerNodeEstimate = NisomersInIsomerspace/std::stoi(getenv("N_TASKS"));
-    auto BatchSize = std::min((int)IsomerPerNodeEstimate, (int)10000);
+    //auto IsomerPerNodeEstimate = NisomersInIsomerspace/std::stoi(getenv("N_TASKS"));
+    auto BatchSize =  std::min((int)NisomersInIsomerspace, (int)1280*10);
 
     size_t Nf = N/2 + 2;
-    std::string filename = "output/full_pipeline_" + std::string(getenv("SLURM_JOB_ID")) + "_" + std::string(getenv("MY_TASK_ID")) + "_" + std::string(getenv("N_TASKS")) + ".csv";
-    ofstream myfile(filename); 
+    //std::string filename = "output/full_pipeline_" + std::string(getenv("SLURM_JOB_ID")) + "_" + std::string(getenv("MY_TASK_ID")) + "_" + std::string(getenv("N_TASKS")) + ".csv";
+    //ofstream myfile(filename); 
 
     sycl::queue Q = sycl::queue(sycl::gpu_selector_v, sycl::property::queue::in_order{});
 
-    size_t n_chunks          = 6; /* Number of chunks per compute node / program instance */ //////3
-    size_t N_chunks          = std::stoi(getenv("N_TASKS"))*n_chunks;                   /* Total number of work chunks */ //
+    //size_t n_chunks          = 6; /* Number of chunks per compute node / program instance */ //////3
+    //size_t N_chunks          = std::stoi(getenv("N_TASKS"))*n_chunks;                   /* Total number of work chunks */ //
 
-    auto my_chunks = loadbalanced_chunks(N_chunks,n_chunks,std::stoi(getenv("MY_TASK_ID")));
-    BuckyGen::buckyherd_queue BuckyQ(N,N_chunks,3,
-				   false,false,my_chunks);
+    //auto my_chunks = loadbalanced_chunks(N_chunks,n_chunks,std::stoi(getenv("MY_TASK_ID")));
+    //BuckyGen::buckyherd_queue BuckyQ(N,N_chunks,3,
+	//			   false,false,my_chunks);
 
     size_t isomers_in_queue = 0;
-    std::vector<node_t> dual_neighbours(IsomerPerNodeEstimate*1.5*Nf*6);
-    std::vector<node_t> face_degrees(IsomerPerNodeEstimate*1.5*Nf); //We do not know a priori how many isomers our buckygen queue will generate so we reserve space for 1.5 times the number of isomers we want to generate.
+    //std::vector<node_t> dual_neighbours(IsomerPerNodeEstimate*1.5*Nf*6);
+    //std::vector<node_t> face_degrees(IsomerPerNodeEstimate*1.5*Nf); //We do not know a priori how many isomers our buckygen queue will generate so we reserve space for 1.5 times the number of isomers we want to generate.
     
     
     Graph G(N);
 
     bool more = true;
 
-    double times_generate; //Times in nanoseconds.
-    double times_memcpy; //Times in nanoseconds.
-    double times_dual; //Times in nanoseconds.
-    double times_tutte; //Times in nanoseconds.
-    double times_project; //Times in nanoseconds.
-    double times_opt; //Times in nanoseconds.
+    double times_generate = 0.; //Times in nanoseconds.
+    double times_memcpy = 0.; //Times in nanoseconds.
+    double times_dual = 0.; //Times in nanoseconds.
+    double times_tutte = 0.; //Times in nanoseconds.
+    double times_project = 0.; //Times in nanoseconds.
+    double times_opt = 0.; //Times in nanoseconds.
+    double times_hessian = 0.; //Times in nanoseconds.
+    double times_spectral_ends = 0.; //Times in nanoseconds.
+    double times_spectral = 0.; //Times in nanoseconds.
+    double times_spectral_vectors = 0.; //Times in nanoseconds.
+
     IsomerBatch<real_t,node_t> batch(N, BatchSize);
+    sycl::buffer<real_t, 1> hessian_buffer(BatchSize*N*90);
+    sycl::buffer<node_t, 1> cols_buffer(BatchSize*N*90);
+    sycl::buffer<real_t, 1> spectral_ends_buffer(BatchSize*2);
+    sycl::buffer<real_t, 1> spectral_buffer(BatchSize*N*3);
+    sycl::buffer<real_t, 1> spectral_vectors_buffer(BatchSize*N*3*N*3);
 
 
-    auto generate_and_fill = [&](IsomerBatch<real_t, node_t>& batch){
+  /*   auto generate_and_fill = [&](IsomerBatch<real_t, node_t>& batch){
         auto isomer_idx = 0;
         sycl::host_accessor acc_dual(batch.dual_neighbours, sycl::write_only);
         sycl::host_accessor acc_degs(batch.face_degrees, sycl::write_only);
@@ -109,12 +119,11 @@ int main(int argc, char** argv) {
             isomer_idx++;
             isomers_in_queue++;
         }
-    };
-
-
-    while(more){
-        auto T1 = std::chrono::steady_clock::now();
-        generate_and_fill(batch);
+    }; */
+    auto Nruns = 10;
+    fill(batch);
+    for(size_t i = 0; i < Nruns; i++){
+        auto T1 = std::chrono::steady_clock::now(); times_generate += std::chrono::duration<double, std::nano>(T1 - T1).count();
         auto T2 = std::chrono::steady_clock::now(); times_generate += std::chrono::duration<double, std::nano>(T2 - T1).count();
         nop_kernel(Q, batch, LaunchPolicy::SYNC);
         auto T3 = std::chrono::steady_clock::now(); times_memcpy += std::chrono::duration<double, std::nano>(T3 - T2).count();
@@ -124,11 +133,20 @@ int main(int argc, char** argv) {
         auto T5 = std::chrono::steady_clock::now(); times_tutte += std::chrono::duration<double, std::nano>(T5 - T4).count();
         spherical_projection(Q, batch, LaunchPolicy::SYNC);
         auto T6 = std::chrono::steady_clock::now(); times_project += std::chrono::duration<double, std::nano>(T6 - T5).count();
-        forcefield_optimize(Q, batch, 5*N, 5*N, LaunchPolicy::SYNC);
+        forcefield_optimize(Q, batch, 4*N, 4*N, LaunchPolicy::SYNC);
         auto T7 = std::chrono::steady_clock::now(); times_opt += std::chrono::duration<double, std::nano>(T7 - T6).count();
+        compute_hessians(Q, batch, hessian_buffer, cols_buffer, LaunchPolicy::SYNC);
+        auto T8 = std::chrono::steady_clock::now(); times_hessian += std::chrono::duration<double, std::nano>(T8 - T7).count();
+        eigensolve<EigensolveMode::ENDS>(Q, batch, hessian_buffer, cols_buffer, spectral_ends_buffer, LaunchPolicy::SYNC, 40);
+        auto T9 = std::chrono::steady_clock::now(); times_spectral_ends += std::chrono::duration<double, std::nano>(T9 - T8).count();
+        eigensolve<EigensolveMode::FULL_SPECTRUM>(Q, batch, hessian_buffer, cols_buffer, spectral_buffer, LaunchPolicy::SYNC);
+        auto T10 = std::chrono::steady_clock::now(); times_spectral += std::chrono::duration<double, std::nano>(T10 - T9).count();
+        //eigensolve<EigensolveMode::FULL_SPECTRUM_VECTORS>(Q, batch, hessian_buffer, cols_buffer, spectral_buffer, LaunchPolicy::SYNC, 40, spectral_vectors_buffer);
+        auto T11 = std::chrono::steady_clock::now(); times_spectral_vectors += std::chrono::duration<double, std::nano>(T11 - T10).count();
+
     }
 
-    myfile << "N, Nf, BatchSize, JOBID, NTASKS, TASK_ID, FILL_ME_UP_SCOTTY, MEMCPY, DUAL, TUTTE, PROJECT, OPT\n" << 
-    N << ", " << Nf << ", " << isomers_in_queue << ", " << getenv("SLURM_JOB_ID") << ", " << getenv("N_TASKS") << ", " << getenv("MY_TASK_ID") << ", " << times_generate/isomers_in_queue << ", " << times_memcpy/isomers_in_queue << ", " << times_dual/isomers_in_queue << ", " << times_tutte/isomers_in_queue << ", " << times_project/isomers_in_queue << ", " << times_opt/isomers_in_queue << "\n";
+    std::cout << "N, Nf, BatchSize, JOBID, NTASKS, TASK_ID, FILL_ME_UP_SCOTTY, MEMCPY, DUAL, TUTTE, PROJECT, OPT\n" << N << ", " << Nf << ", " << (BatchSize*Nruns) << ", " << times_generate/(BatchSize*Nruns) << ", " << times_memcpy/(BatchSize*Nruns) << ", " << times_dual/(BatchSize*Nruns) << ", " << times_tutte/(BatchSize*Nruns) << ", " << times_project/(BatchSize*Nruns) << ", " << times_opt/(BatchSize*Nruns) << ", " << times_hessian/(BatchSize*Nruns) << ", " << times_spectral_ends/(BatchSize*Nruns) << ", " << times_spectral/(BatchSize*Nruns) << ", " << times_spectral_vectors/(BatchSize*Nruns) << std::endl;
+    std::cout << std::endl;
     return 0;
 }
