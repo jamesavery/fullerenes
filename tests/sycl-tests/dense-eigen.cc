e7cc88d79d0e5483554f45d9e11afd244d0003b8
James Avery <avery@nbi.dk>
2024-05-07 11:30:00 +0200

    Work on SYCL QR eigensolver

--- tests/sycl-tests/dense-eigen.cc
+++ tests/sycl-tests/dense-eigen.cc
@@ -5,8 +5,13 @@
 #include <oneapi/dpl/random>
 using namespace sycl;
 
+/* JA: Muy complicado. Få Jonas-tutorial om hvorfor. */
+typedef multi_ptr_t multi_ptr<T, access::address_space::global_space, access::decorated::legacy>;
+
 template <typename T>
-void apply_all_reflections(const sycl::group<1> &cta, const sycl::local_accessor<T,1>& V, const int n, const int m, multi_ptr<T, sycl::access::address_space::global_space, sycl::access::decorated::legacy> Q)
+void apply_all_reflections(const group<1> &cta, 
+                           const local_accessor<T,1>& V, const int n, const int m,                            
+                           multi_ptr_t& Q)
 {   
     static_assert(std::is_floating_point<T>::value, "T must be floating point");
     auto tid = cta.get_local_linear_id();
@@ -25,10 +30,15 @@ void apply_all_reflections(const sycl::group<1> &cta, const sycl::local_accessor
 }
 
 template <typename T>
-void T_QTQ(sycl::group<1>& cta, const int n, const sycl::local_accessor<T,1>& D, const sycl::local_accessor<T,1>& L, const sycl::local_accessor<T,1>& U, const sycl::local_accessor<T,1>& Vout, T shift=0){
-    int tix = cta.get_local_linear_id();
+void T_QTQ(group<1>& cta, 
+            const int n, 
+            const local_accessor<T,1>& D, const local_accessor<T,1>& L, const local_accessor<T,1>& U,
+            const local_accessor<T,1>& Vout, T shift=0)
+{
+    int tix  = cta.get_local_linear_id();
     int bdim = cta.get_local_linear_range();
     FLOAT_TYPEDEFS(T);
+
     //  QTQ_calls ++;
     // Unrolled
     //  T numerical_zero = T.max_norm()*10*std::numeric_limits<T>::epsilon();
@@ -37,11 +47,11 @@ void T_QTQ(sycl::group<1>& cta, const int n, const sycl::local_accessor<T,1>& D,
     for (int i = tix; i < n; i += bdim){
         local_max = std::max(local_max, abs(D[i]) + 2*abs(L[i]));
     }
-    T max_norm = reduce_over_group(cta, local_max, sycl::maximum<T>());
+    T max_norm = reduce_over_group(cta, local_max, maximum<T>());
     T numerical_zero = 10*std::numeric_limits<T>::epsilon();
     T d_n, l_n, l_nm1;
     d_n = D[n]; l_n = L[n]; l_nm1 = L[n-1];
-    sycl::group_barrier(cta);
+    group_barrier(cta);
     //T a[2], v[2], D[n+1], L[n+1], U[2*(n+1)];
     T a[2], v[2];//, D[n+1], L[n+1], U[2*(n+1)];
     for(int k = tix; k < n + 1; k += bdim){
@@ -56,10 +66,10 @@ void T_QTQ(sycl::group<1>& cta, const int n, const sycl::local_accessor<T,1>& D,
         }
     }
 
-    sycl::group_barrier(cta);
+    group_barrier(cta);
     if(tix == 0)
         for(int k=0;k<n-1;k++){
-            if (sycl::abs(L[k]) > numerical_zero){
+            if (abs(L[k]) > numerical_zero){
             a[0] = D[k]; a[1] = L[k];       // a = T[k:k+2,k] is the vector of nonzeros in kth subdiagonal column.
             
             T anorm = sqrt(a[0]*a[0] + a[1]*a[1]); 
@@ -105,7 +115,7 @@ void T_QTQ(sycl::group<1>& cta, const int n, const sycl::local_accessor<T,1>& D,
         L[k]       -= T(2.)*v[0]*vTA[1]; // T(k+1,k)   -= 2*v[0]*vTA[1]
         D[k+1]     -= T(2.)*v[1]*vTA[1]; // T(k+1,k+1) -= 2*v[1]*vTA[1]        
     }
-    sycl::group_barrier(cta);
+    group_barrier(cta);
 
 
     if(tix == 0){
@@ -125,22 +135,22 @@ void T_QTQ(sycl::group<1>& cta, const int n, const sycl::local_accessor<T,1>& D,
     }
 
 
-    sycl::group_barrier(cta);
+    group_barrier(cta);
     for (int k = tix; k<n; k+=bdim){  // Copy working diagonals to output
         D[k] += shift;
         if(k < n-1){
             L[k] = U[k];
         }
     }
-    sycl::group_barrier(cta);
+    group_barrier(cta);
     if (tix==0){
         D[n] = d_n;
         L[n-1] = l_nm1;
         L[n] = l_n;
     }
-    sycl::group_barrier(cta);
-
+    group_barrier(cta);
 }
+
 
 template <typename T>
 std::array<T,2> eigvalsh2x2(const std::array<T,4> &A){
@@ -150,70 +160,77 @@ std::array<T,2> eigvalsh2x2(const std::array<T,4> &A){
 }
 
 template <typename T>
-T max_norm(const sycl::local_accessor<T,1>& A, const int m, n){
-{
+T max_norm(const group<1>& cta, 
+           const local_accessor<T,1>& A, const int m, const int n)
+{//TODO: Regular segmented reduce -> m*n parallel
   T mx = 0;
   int j = cta.get_local_id(0);
   for(int i=0;i<m;i++){ 	
-    T row_norm = sycl::reduce_over_group(cta, sycl::abs(A[i*n+j]), sycl::plus<T>());
-    mx = sycl::max(mx,row_norm);
+    T row_norm = reduce_over_group(cta, abs(A[i*n+j]), plus<T>());
+    mx = max(mx,row_norm);
   }
   return mx;  
 }
 
 template <typename T>
-T reflection_vector(const T a,const T anorm)
+T reflection_vector(const group<1>& cta,
+                    const T& a_i,const T& anorm)
 {
-    int tid = cta.get_local_id(0);
-    T alpha = -copysign(anorm,a);
-    T v = a + (tid==0)*alpha; // TODO: Check fortegn 
-    T vnorm = sycl::sqrt(sycl::reduce_over_group(cta, v*v, sycl::plus<T>()));
-    return v / vnorm;
+    int i_tid = cta.get_local_id(0);
+    T alpha = -copysign(anorm,a_i);
+    T v_i = a_i + (i_tid==0)*alpha; // TODO: Check fortegn 
+    T vnorm = sqrt(reduce_over_group(cta, v_i*v_i, plus<T>()));
+    return v_i / vnorm;
 }
 
 /* TODO: Kan jeg håndtere a[0] =~= 0 mere robust? Kan jeg inkludere pivots? */
 /* Note: Restricted to real matrix A, redo for complex. */
 template <typename T>
-void QHQ(sycl::group<1>& cta, /*in/out*/T *A, int n, T *Q)
+void QHQ(const group<1>& cta, 
+         /*in/out*/local_accessor<T,1>& A, int n, 
+         /*in/out*/local_accessor<T,1>& Q,
+         /*workmem*/local_accessor<T,1>& vHA_wm)
 {
   T numerical_zero = max_norm(A,n,n)*10*std::numeric_limits<T>::epsilon();
   
-  int tid = cta.get_local_id(0);
+  int j_tid = cta.get_local_id(0);
   for(int k=0;k<n-1;k++){ // Sequential loop
-    int l = n-(k+1);		         /* length of kth subdiagonal column */
-    T a     = tid<l? A[k*n+(k+1)+j_tid] : 0;      /* a = A[k,(k+1):n], kth postdiagonal row. */
-    T anorm = sycl::sqrt(sycl::reduce_over_group(cta, a*a, sycl::plus<T>())); /* Norm of a */
+    int l = n-(k+1);		                   /* length of kth postdiagonal row a */
+    T a_i   = j_tid<l? A[k*n+(k+1)+j_tid] : 0; /* a = A[k,(k+1):n], kth postdiagonal row. */
+    T anorm = sqrt(reduce_over_group(cta, a_i*a_i, plus<T>())); /* Norm of a */
 
     if(anorm < numerical_zero) continue; /* Already eliminated, don't divide by 0 */
 
-    v =  reflection_vector(a,anorm);    /* Vector definining elimination operations */
+    T v_i =  reflection_vector(cta, a_i, anorm);    /* Vector definining elimination operations */
     
-    reflect_region(A,n,/* reg. start:*/k+1,k, /* reg. size:*/l,l+1, v,2, ROWS);/* TODO: fix */
-    reflect_region(A,n,/* reg. start:*/k+1,k, /* reg. size:*/l,l+1, v,2, COLS);
+    reflect_region(cta,A,n,/* reg. start:*/k+1,k, /* reg. size:*/l,l+1, v_i,2, ROWS, vHA_wm);
+    reflect_region(cta,A,n,/* reg. start:*/k+1,k, /* reg. size:*/l,l+1, v_i,2, COLS, vHA_wm);
 
-    if(Q != 0) reflect_region(Q,n, k+1,0, l,n, v, sigma, ROWS); 
+    if(Q != 0) reflect_region(cta,Q,n, k+1,0, l,n, v_i, sigma, ROWS, vHA_wm); 
   }
 }
 
-//A[i,j] = A[i,j] - sum(2*v[i]*A[j,k]*v[k],k)
+
+// sum(2*v[i]*A[j,k]*v[k],k) = 2*v[i]*sum(A[j,k]*v[k],k)
 template <typename T>
-void reflect_region(sycl::local_accessor<T,1>& A, int cols)
+void reflect_region(/*in/out*/local_accessor<T,1> &A,
+    int N, int i0, int j0, int m, int n, const T &v_i, int cols,
+    local_accessor<T,1>& vHA)
 {
-  T vHA_j;
-  int stride[2] = {N*(!cols) + 1*cols, N*cols + 1*(!cols)};
+    int stride[2] = {N * (!cols) + 1 * cols, N * cols + 1 * (!cols)};
 
-  int i_tid = cta.get_local_id(0);
-  for(int j=0;j<n;j++){
-    size_t IJ = (i_tid+i0)*stride[0] + (j+j0)*stride[1];
-    T vAij = (i_tid<m)? v[i]*A[IJ] : 0;
-    T val = sycl::reduce_over_group(cta, vAij, sycl::plus<T>());
-    if(i_tid==j) vHA_j = val;
-  }
+    int i_tid = cta.get_local_id(0);
+    for (int j = 0; j < n; j++) // TODO: Implement segmented sum
+    {
+        size_t IJ = (i + i0) * stride[0] + (j + j0) * stride[1];
+        T vAij = v_i*A[IJ];
+        T sum = reduce_over_group(cta, vAij, plus<T>());
+        vHA[j] = sum;
+    }
 
-  for(size_t i=0;i<m;i++){       /* A += -2*outer(v,vTA) */
-    int j_tid = cta.get_local_id(0);
-    size_t IJ = (i+i0)*stride[0] + (j_tid+j0)*stride[1];
-    if(tid<n) A[IJ] -= 2*v[i]*vHA_j; 
-  }  
+    for (size_t j = 0; j < n; j++) /* A += -2*outer(v,vTA) */
+    {
+        size_t IJ = (i_tid + i0) * stride[0] + (j + j0) * stride[1];
+        A[IJ] -= 2 * v_i * vHA[j];
+    }
 }
-
