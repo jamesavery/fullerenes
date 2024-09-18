f521141e455376ef73cfac64524ec7c39c2f8da9
jonasdelacour <jonaslacour1@gmail.com>
2024-09-18 13:30:21 +0200

    Made baseline pipeline work with the new interfaces.

--- benchmarks/sycl/baseline_pipeline.cc
+++ benchmarks/sycl/baseline_pipeline.cc
@@ -38,7 +38,7 @@ int main(int argc, char** argv) {
     std::string device_type = argc>3 ? argv[3] : "gpu";
     size_t Nruns = argc>4 ? strtol(argv[4],0,0) : 10;
 
-    size_t BatchSize = std::ceil((real_t)NumNodes/(real_t)N);
+    size_t BatchSize = 1280; //std::ceil((real_t)NumNodes/(real_t)N);
 
     //auto selector =  device_type == "cpu" ? sycl::cpu_selector_v : sycl::gpu_selector_v;
     auto device = Device(device_type);
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
     SyclQueue Q(device);
     
     FullereneBatch<real_t,node_t> batch(N, BatchSize);
-    Graph G(N);
+    Graph G(Nf, true);
     auto fill_and_dualize = [&](FullereneBatch<real_t,node_t>& batch, double& filltime, double& dualtime)
     {
     BuckyGen::buckygen_queue BuckyQ = BuckyGen::start(N, 0, 0);
@@ -57,28 +57,21 @@ int main(int argc, char** argv) {
     {   
         auto start = std::chrono::steady_clock::now();
         auto more = BuckyGen::next_fullerene(BuckyQ, G);
-        batch.push_back(G, ii);
+        //batch.push_back(G, ii);
         FullereneDual FD(G);
         auto T0 = std::chrono::steady_clock::now(); ftime += std::chrono::duration<double, std::nano>(T0 - start).count();
         FD.update();
         PlanarGraph pG = FD.dual_graph();
-        auto acc_cubic = batch.d_.A_cubic_;
-        for (size_t j = 0; j < N; j++){
-            for (size_t k = 0; k < 3; k++)
-            {
-                acc_cubic[ii*N*3 + j*3 + k] = pG.neighbours[j][k];
-            }
-            
-        }
+        batch.push_back(pG, ii);
         auto T1 = std::chrono::steady_clock::now(); dtime += std::chrono::duration<double, std::nano>(T1 - T0).count();
         if(!more) break;
 
         
         batch.m_.flags_[ii] |= StatusFlag::CUBIC_INITIALIZED;
     }
+    BuckyGen::stop(BuckyQ);
     filltime = ftime;
     dualtime = dtime;
-    BuckyGen::stop(BuckyQ);
     };
     
     vector<double> times_generate(Nruns); //Times in nanoseconds.
@@ -95,6 +88,7 @@ int main(int argc, char** argv) {
 
     for(int i = 0; i < Nruns; i++){
         auto start = std::chrono::steady_clock::now();
+        FullereneBatch<real_t,node_t> batch(N, BatchSize);
         fill_and_dualize(batch, times_generate[i], times_dual[i]);
         auto T0 = std::chrono::steady_clock::now();
         //nop_kernel(Q, batch, LaunchPolicy::SYNC);
