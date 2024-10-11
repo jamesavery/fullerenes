1c719278c95d434260c083d2057fae31f58d7bf3
jonasdelacour <jonaslacour1@gmail.com>
2024-10-11 11:30:49 +0200

    Refactor nop_kernel to use FullereneBatch and FullereneBatchView internally

--- src/sycl/nop_kernel.cc
+++ src/sycl/nop_kernel.cc
@@ -4,41 +4,37 @@
 #include <tuple>
 #include <iterator>
 #include <type_traits>
-#include <fullerenes/sycl-isomer-batch.hh>
 #include "forcefield-includes.cc"
+#include <fullerenes/sycl-headers/misc-enums.hh>
+#include <fullerenes/sycl-headers/sycl-fullerene-structs.hh>
 
 template <typename T, typename K>
-void nop_kernel(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy policy){
+void nop_kernel(sycl::queue&Q, FullereneBatch<T,K>& batch, const LaunchPolicy policy){
     TEMPLATE_TYPEDEFS(T,K);
     
+    auto batch_view = FullereneBatchView<T,K>(batch);
+
     if(policy == LaunchPolicy::SYNC) Q.wait();
     Q.submit([&](handler &h) {
-        auto N = batch.N();
-        auto Nf = batch.Nf();
+        auto N = batch.N_;
+        auto Nf = batch.Nf_;
         auto capacity = batch.capacity();
     
 
         auto num_compute_units = Q.get_device().get_info<info::device::max_compute_units>();
         auto n_blocks_strided = num_compute_units;
         //Create device accessors
-        accessor     cubic_neighbours_dev(batch.cubic_neighbours, h, read_write);
-        accessor     face_degrees_dev(batch.face_degrees, h, read_write);
-        accessor     dual_neighbours_dev(batch.dual_neighbours, h, read_write);
-        accessor     xys_dev(batch.xys, h, read_write);
-        accessor     X_dev(batch.X, h, read_write);
-        accessor     statuses_dev(batch.statuses, h, read_write);
-        accessor     iterations_dev(batch.iterations, h, read_write);
 
         h.parallel_for<class dualize>(sycl::nd_range(sycl::range{N*capacity}, sycl::range{N}), [=](nd_item<1> nditem) {
             auto i = nditem.get_global_id(0);
             auto bid = nditem.get_group_linear_id();
-            auto cn = cubic_neighbours_dev[i];
-            auto fd = face_degrees_dev[i];
-            auto dn = dual_neighbours_dev[i];
-            auto xy = xys_dev[i];
-            auto X = X_dev[i];
-            auto s = statuses_dev[i];
-            auto it = iterations_dev[i];
+            auto cn = batch_view.d_.A_cubic_[i];
+            auto fd = batch_view.d_.deg_[i];
+            auto dn = batch_view.d_.A_dual_[i];
+            auto xy = batch_view.d_.X_cubic_[i];
+            auto X = batch_view.d_.X_dual_[i];
+            auto s = batch_view.m_.flags_[i];
+            auto it = batch_view.m_.iterations_[i];
             //Do nothing.
             
         });
@@ -46,4 +42,4 @@ void nop_kernel(sycl::queue&Q, IsomerBatch<T,K>& batch, const LaunchPolicy polic
     if(policy == LaunchPolicy::SYNC) Q.wait();
 }
 
-template void nop_kernel<float,uint16_t>(sycl::queue&, IsomerBatch<float,uint16_t>&, const LaunchPolicy);
+template void nop_kernel<float,uint16_t>(sycl::queue&, FullereneBatch<float,uint16_t>&, const LaunchPolicy);
