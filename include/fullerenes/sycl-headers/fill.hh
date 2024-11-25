ffcfd52dacddbdcd2a405c21d0403aa849664d0a
jonasdelacour <jonaslacour1@gmail.com>
2024-11-25 14:34:50 +0100

    Remove commented-out code in fill function.

--- include/fullerenes/sycl-headers/fill.hh
+++ include/fullerenes/sycl-headers/fill.hh
@@ -26,14 +26,6 @@ void fill(FullereneBatch<T,K>& B, int mytask_id = 0, int ntasks = 1) {
   BuckyGen::stop(BuckyQ);
   if (num_generated < N_graphs) 
     for (int i = num_generated; i < N_graphs; ++i) {
-      /* statuses_acc[i] = StatusEnum::DUAL_INITIALIZED;
-      //Repeat the same graphs as already generated.
-      for(int j = 0; j < Nf; j++) {
-        face_degrees_acc[i*Nf + j] = face_degrees_acc[(i%num_generated)*Nf + j];
-        for(int k = 0; k < 6; k++) 
-          dual_neighbours_acc[i*Nf + j][k] = dual_neighbours_acc[(i%num_generated)*Nf + j][k];
-
-      } */
       if(i < B.size()) decltype(B[i])::copy(B[i], B[i%num_generated]);
       else B.push_back(B[i%num_generated]);
     }
