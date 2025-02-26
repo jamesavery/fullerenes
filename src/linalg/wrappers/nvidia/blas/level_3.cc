2bea4da53a3f139c5919699ebf72db05286a5c0e
jonasdelacour <jonaslacour1@gmail.com>
2025-02-26 15:35:55 +0100

    Added syev support in the CUDA backend

--- src/linalg/wrappers/nvidia/blas/level_3.cc
+++ src/linalg/wrappers/nvidia/blas/level_3.cc
@@ -116,12 +116,176 @@ namespace linalg {
         return ctx.get_event();
         
     }
+
+    template <Backend B, typename T, Format F, BatchType BT>
+    SyclEvent spmm(SyclQueue& ctx,
+                   SparseMatHandle<T, F, BT>& descrA,
+                   DenseMatHandle<T,BT>& descrB,
+                   DenseMatHandle<T,BT>& descrC,
+                   T alpha,
+                   T beta,
+                   Transpose transA,
+                   Transpose transB,
+                   Span<std::byte> workspace) {
+        // Call cuBLAS
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+
+        BumpAllocator pool(workspace);
+        auto buffer_size = spmm_buffer_size<B>(ctx, descrA, descrB, descrC, alpha, beta, transA, transB);
+        auto buffer = pool.allocate<std::byte>(ctx, buffer_size);
+
+        cusparseSpMM(
+            handle,
+            enum_convert<BackendLibrary::CUSPARSE>(transA),//transA == Transpose::NoTrans ? CUSPARSE_OPERATION_NON_TRANSPOSE : CUSPARSE_OPERATION_TRANSPOSE,
+            enum_convert<BackendLibrary::CUSPARSE>(transB),//transB == Transpose::NoTrans ? CUSPARSE_OPERATION_NON_TRANSPOSE : CUSPARSE_OPERATION_TRANSPOSE,
+            &alpha,
+            *descrA,
+            *descrB,
+            &beta,
+            *descrC,
+            BackendScalar<T,B>::type,
+            CUSPARSE_SPMM_ALG_DEFAULT,
+            buffer.data()
+        );
+        return ctx.get_event();
+    }
+
+    template <Backend B, typename T, Format F, BatchType BT>
+    size_t spmm_buffer_size(SyclQueue& ctx,
+                               SparseMatHandle<T, F, BT>& descrA,
+                               DenseMatHandle<T,BT>& descrB,
+                               DenseMatHandle<T,BT>& descrC,
+                               T alpha,
+                               T beta,
+                               Transpose transA,
+                               Transpose transB) {
+        // Call cuBLAS
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+
+        size_t size = 0;
+
+        cusparseSpMM_bufferSize(
+            handle,
+            enum_convert<BackendLibrary::CUSPARSE>(transA),
+            enum_convert<BackendLibrary::CUSPARSE>(transB),
+            &alpha,
+            *descrA,
+            *descrB,
+            &beta,
+            *descrC,
+            BackendScalar<T,B>::type,
+            CUSPARSE_SPMM_ALG_DEFAULT,
+            &size
+        );
+        return size;
+    }
+
+    template <Backend B, typename T, BatchType BT>
+    SyclEvent syev(SyclQueue& ctx,
+                   DenseMatHandle<T,BT>& descrA,
+                   Span<T> eigenvalues,
+                   Uplo uplo,
+                   Span<std::byte> workspace) {
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+        BumpAllocator pool(workspace);
+
+        size_t l_work_device;
+        size_t l_work_host;
+        
+        cusolverDnParams_t params;
+        cusolverDnCreateParams(&params);
+        if constexpr (BT == BatchType::Single){
+            cusolverDnXsyevd_bufferSize(handle, params, CUSOLVER_EIG_MODE_VECTOR, enum_convert<BackendLibrary::CUSOLVER>(uplo), descrA.rows_, BackendScalar<T,B>::type, descrA.data_, descrA.ld_, BackendScalar<T,B>::type, eigenvalues.data(), BackendScalar<T,B>::type, &l_work_device, &l_work_host);
+        } else {
+            cusolverDnXsyevBatched_bufferSize(handle, params, CUSOLVER_EIG_MODE_VECTOR, enum_convert<BackendLibrary::CUSOLVER>(uplo), descrA.rows_, BackendScalar<T,B>::type, descrA.data_, descrA.ld_, BackendScalar<T,B>::type, eigenvalues.data(), BackendScalar<T,B>::type, &l_work_device, &l_work_host, descrA.batch_size_);
+        }
+        
+        auto host_workspace = pool.allocate<std::byte>(ctx, l_work_host);
+        auto device_workspace = pool.allocate<std::byte>(ctx, l_work_device);
+                                                       //auto syev_span = pool.allocate<std::byte>(ctx, Lwork);
+        if constexpr (BT == BatchType::Single) {
+
+            int info;
+            cusolverDnXsyevd(
+                handle,
+                params,
+                CUSOLVER_EIG_MODE_VECTOR,
+                enum_convert<BackendLibrary::CUSOLVER>(uplo),
+                descrA.rows_,
+                BackendScalar<T,B>::type,
+                get_data(descrA),
+                descrA.ld_,
+                BackendScalar<T,B>::type,
+                eigenvalues.data(),
+                BackendScalar<T,B>::type,
+                device_workspace.data(),
+                l_work_device,
+                host_workspace.data(),
+                l_work_host,
+                &info);
+        } else {
+            auto info = pool.allocate<int>(ctx, descrA.batch_size_);
+            cusolverDnXsyevBatched(
+                handle,
+                params,
+                CUSOLVER_EIG_MODE_VECTOR,
+                enum_convert<BackendLibrary::CUSOLVER>(uplo),
+                descrA.rows_,
+                BackendScalar<T,B>::type,
+                descrA.data_,
+                descrA.ld_,
+                BackendScalar<T,B>::type,
+                eigenvalues.data(),
+                BackendScalar<T,B>::type,
+                device_workspace.data(),
+                l_work_device,
+                host_workspace.data(),
+                l_work_host,
+                info.data(),
+                descrA.batch_size_);
         }
         return ctx.get_event();
     }
 
-    
+    template <Backend B, typename T, BatchType BT>
+    size_t syev_buffer_size(SyclQueue& ctx,
+                            DenseMatHandle<T,BT>& descrA,                            
+                            Span<T> eigenvalues,
+                            Uplo uplo) {
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+        size_t l_work_device;
+        size_t l_work_host;
+        size_t total = 0;
+        cusolverDnParams_t params;
+        cusolverDnCreateParams(&params);
+        if constexpr (BT == BatchType::Single){
+            cusolverDnXsyevd_bufferSize(handle, params, CUSOLVER_EIG_MODE_VECTOR, enum_convert<BackendLibrary::CUSOLVER>(uplo), descrA.rows_, BackendScalar<T,B>::type, descrA.data_, descrA.ld_, BackendScalar<T,B>::type, eigenvalues.data(), BackendScalar<T,B>::type, &l_work_device, &l_work_host);
+        } else {
+             cusolverDnXsyevBatched_bufferSize(handle, params, CUSOLVER_EIG_MODE_VECTOR, enum_convert<BackendLibrary::CUSOLVER>(uplo), descrA.rows_, BackendScalar<T,B>::type, descrA.data_, descrA.ld_, BackendScalar<T,B>::type, eigenvalues.data(), BackendScalar<T,B>::type, &l_work_device, &l_work_host, descrA.batch_size_);
+             total = BumpAllocator::allocation_size<int>(ctx, descrA.batch_size_);
+        }
 
+        return BumpAllocator::allocation_size<std::byte>(ctx, l_work_host) + BumpAllocator::allocation_size<std::byte>(ctx, l_work_device) + total;
+    }
+
+    template <Backend B, typename T, BatchType BT>
+    SyclEvent ortho(SyclQueue& ctx,
+                    DenseMatHandle<T,BT>& A,
+                    Transpose transA,
+                    Span<std::byte> workspace,
+                    OrthoAlgorithm algo) {
+        static LinalgHandle<B> handle;
+        handle.setStream(ctx);
+        BumpAllocator pool(workspace);
+        //auto buffer_size = ortho_buffer;
+        size_t size = 0;
+
+
+    }
 
                    
     #define GEMM_INSTANTIATE(fp, BT) \
@@ -151,13 +315,48 @@ namespace linalg {
         SyclQueue&, \
         DenseMatHandle<fp, BT>&, \
         Uplo);
+
+    #define SPMM_INSTANTIATE(fp, F, BT) \
+    template SyclEvent spmm<Backend::CUDA, fp, F, BT>( \
+        SyclQueue&, \
+        SparseMatHandle<fp, F, BT>&, \
+        DenseMatHandle<fp, BT>&, \
+        DenseMatHandle<fp, BT>&, \
+        fp, fp, Transpose, Transpose, Span<std::byte>);
+    
+    #define SPMM_BUFFER_SIZE_INSTANTIATE(fp, F, BT) \
+    template size_t spmm_buffer_size<Backend::CUDA, fp, F, BT>( \
+        SyclQueue&, \
+        SparseMatHandle<fp, F, BT>&, \
+        DenseMatHandle<fp, BT>&, \
+        DenseMatHandle<fp, BT>&, \
+        fp, fp, Transpose, Transpose);
+
+    #define SYEV_INSTANTIATE(fp, BT) \
+    template SyclEvent syev<Backend::CUDA, fp, BT>( \
+        SyclQueue&, \
+        DenseMatHandle<fp, BT>&, \
+        Span<fp>, \
+        Uplo, \
+        Span<std::byte>);
+
+    #define SYEV_BUFFER_SIZE_INSTANTIATE(fp, BT) \
+    template size_t syev_buffer_size<Backend::CUDA, fp, BT>( \
+        SyclQueue&, \
+        DenseMatHandle<fp, BT>&, \
+        Span<fp>, \
+        Uplo);
     
     #define BLAS_INSTANTIATE(fp, BT) \
         GEMM_INSTANTIATE(fp, BT) \
         TRSM_INSTANTIATE(fp, BT) \
         POTRF_INSTANTIATE(fp, BT) \
-        POTRF_BUFFER_SIZE_INSTANTIATE(fp, BT)
-    
+        POTRF_BUFFER_SIZE_INSTANTIATE(fp, BT) \
+        SPMM_INSTANTIATE(fp, Format::CSR, BT) \
+        SPMM_BUFFER_SIZE_INSTANTIATE(fp, Format::CSR, BT) \
+        SYEV_INSTANTIATE(fp, BT) \
+        SYEV_BUFFER_SIZE_INSTANTIATE(fp, BT) 
+        
     // Macro that covers all layout and batch type combinations for a given floating-point type.
     #define GEMM_INSTANTIATE_FOR_FP(fp)                \
         BLAS_INSTANTIATE(fp, BatchType::Batched)        \
