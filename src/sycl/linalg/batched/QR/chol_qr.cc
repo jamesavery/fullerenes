b5d86261294dcec500a2b8e525bfa0a084092240
jonasdelacour <jonaslacour1@gmail.com>
2025-02-26 15:36:50 +0100

    feat: implement shift_chol3_qr_batched function and buffer size calculation for CUDA support

--- src/sycl/linalg/batched/QR/chol_qr.cc
+++ src/sycl/linalg/batched/QR/chol_qr.cc
@@ -42,8 +42,6 @@ void chol_qr_batched(   SyclQueue& ctx,
     trsm<Backend::CUDA>(ctx, descrC, descrA, Side::Right, Uplo::Lower, Transpose::Trans, Diag::NonUnit, alpha);
     //Compute the QR factorization of Q
     gemm<Backend::CUDA>(ctx, descrA, descrA, descrC, alpha, beta, Transpose::Trans, Transpose::NoTrans);
-    
-    print_matrix(ATA, n, n);
     ctx.wait();
 }
 
@@ -94,67 +92,68 @@ void shift_chol3_qr_batched(SyclQueue& ctx,
                         Span<std::byte> workspace){
     constexpr T alpha = 1.0;
     constexpr T beta = 0.0;
+    
+    BumpAllocator pool(workspace);
+    auto ATA = pool.allocate<T>(ctx, n*n*batch_size);
+    
+    auto ATA_stride = n*n;
+    auto descrA = DenseMatHandle<T, BatchType::Batched>(A.data(), m, n, m, Astride, batch_size);
+    auto descrC = DenseMatHandle<T, BatchType::Batched>(ATA.data(), n, n, n, ATA_stride, batch_size);
+    auto potrf_workspace = pool.allocate<std::byte>(ctx, potrf_buffer_size<Backend::CUDA>(ctx, descrC, Uplo::Lower));
 
-    //Compute StS = S^T * S
-    //cublasSgemmStridedBatched(blas_handle, CUBLAS_OP_T, CUBLAS_OP_N,
-    //                            SN, /* m */
-    //                            SN, /* n */
-    //                            m, /* k */
-    //                            &alpha, /* alpha */
-    //                            S_ptrs.front(), /* A */
-    //                            m, /* lda */
-    //                            S_stride, /* strideA */
-    //                            S_ptrs.front(), /* B */
-    //                            m, /* ldb */
-    //                            S_stride, /* strideB */
-    //                            &beta, /* beta */
-    //                            STS_ptrs.front(), /* C */
-    //                            SN, /* ldc */
-    //                            STS_stride, /* strideC */
-    //                            batch_size);
+    gemm<Backend::CUDA>(ctx, descrA, descrA, descrC, alpha, beta, Transpose::Trans, Transpose::NoTrans);
 
-    /* ctx -> submit([&](sycl::handler& h){
-        h.parallel_for(nd_range<1>(sycl::range{size_t(batch_size*SN)}, sycl::range{size_t(SN)}), [=](sycl::nd_item<1> item){
+    auto ATA_ptrs = get_data(descrC);
+    ctx -> submit([&](sycl::handler& h){
+        h.parallel_for(sycl::nd_range<1>(sycl::range{size_t(batch_size*n)}, sycl::range{size_t(n)}), [=](sycl::nd_item<1> item){
             auto tid = item.get_local_linear_id();
             auto bid = item.get_group_linear_id();
             auto cta = item.get_group();
-            auto STS_acc = STS_ptrs[bid];
+            auto ATA_acc = ATA_ptrs[bid];
             
-            auto g_norm = sycl::reduce_over_group(cta, sycl::sqrt(STS_acc[tid*SN + tid]), sycl::maximum<T>());
+            auto g_norm = sycl::reduce_over_group(cta, sycl::sqrt(ATA_acc[tid*n + tid]), sycl::maximum<T>());
             auto eps = std::numeric_limits<T>::epsilon();
-            auto shift = T(11.0) * (m*SN*eps +(SN + 1)*SN*eps) * g_norm;
-            STS_acc[tid*SN + tid] += shift;
+            auto shift = T(11.0) * (m*n*eps + (n + 1)*n*eps) * g_norm;
+            ATA_acc[tid*n + tid] += shift;
         });
     });
     ctx.wait();
 
     //Compute the Cholesky Factorization of StS
-    cusolverDnSpotrfBatched(solver_handle, CUBLAS_FILL_MODE_LOWER, SN, STS_ptrs.data(), SN, d_info.data(), batch_size);
-    //cublasSgetrfBatched(blas_handle, SN, Sts_ptrs.data(), SN, NULL, d_info.data(), batch_size);
-    cudaDeviceSynchronize();
-    //Compute Q = S * StS^-1
-    cublasStrsmBatched( blas_handle,
-                        CUBLAS_SIDE_RIGHT, 
-                        CUBLAS_FILL_MODE_LOWER, 
-                        CUBLAS_OP_T, 
-                        CUBLAS_DIAG_NON_UNIT, 
-                        m, 
-                        SN, 
-                        &alpha, 
-                        STS_ptrs.data(), 
-                        SN, 
-                        S_ptrs.data(), 
-                        m, batch_size);
-    cudaDeviceSynchronize();
-
-
+    potrf<Backend::CUDA>(ctx, descrC, Uplo::Lower, potrf_workspace);
+    trsm<Backend::CUDA>(ctx, descrC, descrA, Side::Right, Uplo::Lower, Transpose::Trans, Diag::NonUnit, alpha);
 
     chol_qr_batched(ctx, transpose_op, batch_size, Astride, m, n, A, workspace);
-    chol_qr_batched(ctx, transpose_op, batch_size, Astride, m, n, A, workspace); */
+    chol_qr_batched(ctx, transpose_op, batch_size, Astride, m, n, A, workspace);
 } 
+
+
+template <typename T>
+size_t shift_chol3_qr_batched_buffer_size(SyclQueue& ctx,
+                        bool transpose_op,
+                        int batch_size,
+                        int Astride,
+                        int m,
+                        int n,
+                        Span<T> A){
+    return chol_qr_batched_buffer_size(ctx, transpose_op, batch_size, Astride, m, n, A) + 
+            BumpAllocator::allocation_size<T>(ctx, n*n*batch_size);
+}
 
 //Instantiate the template
 template void chol_qr_batched<float>(SyclQueue&,bool,int,int,int,int,Span<float>,Span<std::byte> workspace);
 
 //Instantiate the template
 template size_t chol_qr_batched_buffer_size<float>(SyclQueue&,bool,int,int,int,int,Span<float>);
+
+//Instantiate the template
+template void chol2_qr_batched<float>(SyclQueue&,bool,int,int,int,int,Span<float>,Span<std::byte> workspace);
+
+//Instantiate the template
+template size_t chol2_qr_batched_buffer_size<float>(SyclQueue&,bool,int,int,int,int,Span<float>);
+
+//Instantiate the template
+template void shift_chol3_qr_batched<float>(SyclQueue&,bool,int,int,int,int,Span<float>,Span<std::byte> workspace);
+
+//Instantiate the template
+template size_t shift_chol3_qr_batched_buffer_size<float>(SyclQueue&,bool,int,int,int,int,Span<float>);
