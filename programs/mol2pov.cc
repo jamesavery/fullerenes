4fa3f29c7f1582d78eab09a5eeaca07da45b32fb
James Avery <avery@diku.dk>
2026-03-05 14:11:36 +0100

    mol2pov fix

--- programs/mol2pov.cc
+++ programs/mol2pov.cc
@@ -16,15 +16,14 @@ int main(int ac, char **av)
   for(int i=1;i<ac;i++){
     string filename(av[i]);
     string outname(filename.substr(0,filename.rfind("."))+".pov");
-    //  cerr << "Reading from: " << filename << endl
-    //       << "Writing to:   " << outname  << endl;
+    cerr << "Reading from: " << filename << endl;
 
-    Polyhedron P(filename);
+    Polyhedron P = P::from_file(filename);
 
     double min_diameter = 4, max_diameter = 30, diameter = P.diameter();
     double width = 2.5*diameter/min_diameter;
 
-    //  printf("{%d, %f, %f}\n",G.N,P.volume(),P.diameter());
+    printf("{%d, %f, %f}\n",G.N,P.volume(),P.diameter());
 
     ofstream outfile(outname.c_str());
     int red   = int(256.0*min(diameter/max_diameter-0.1,0.8));
@@ -35,6 +34,7 @@ int main(int ac, char **av)
 
     outfile << P.to_povray(width,width,0x6a5acd,vertex_colour);
     outfile.close();
+    cerr << "Wrote to:   " << outname  << endl;    
   }
 
   return 0;
